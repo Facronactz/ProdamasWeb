0ff14d7783bc31b8260eca4128b332c6b8bdf18e
Facronactz <facronactz@gmail.com>
2022-03-09 13:01:28 +0700

    update view

--- resources/views/artikel/artikelLay.blade.php
+++ resources/views/artikel/artikelLay.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.desain')
-@section('content')
+@section('artikel', 'active')
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-  <title>Prodamas Plus</title>
-
-  <!--Goofle Font-->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap" rel="stylesheet" />
-
-  <!--font awesome-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        
+        <title>Prodamas Plus</title>
+        
+        <!--Goofle Font-->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap" rel="stylesheet" />
+        
+        <!--font awesome-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+        
   <!--CSS-->
+  @section('head')
   <link rel="stylesheet" href="{{ asset('/css/artikel.css') }}">
-
+  @endsection
 </head>
 
 <body>
-<!-- Content -->
-<div class="row">
+    <!-- Content -->
+    @section('content')
+    <div class="row">
         <div class="leftcolumn mt-5">
             @foreach ($artikel as $item)    
             <div class="card">
@@ -75,6 +77,6 @@
         </div>
     </div>
 <!-- Akhir content -->
+@endsection
 </body>
 </html>
-@endsection
