835f19439c5b89c3aee8becdddbc70ea3e4e6f8c
Farro Axza <farroaxza@gmail.com>
2022-03-12 18:50:41 +0700

    cleaning code

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -1,42 +1,11 @@
 @extends('layouts.desain')
 @section('artikel','active')
-{{-- @section('search') --}}
-{{-- <form class="d-flex">
-    <input class="form-control me-2" type="search"
-        style="font-family: 'Font Awesome 5 Free'; font-weight: 600; border-radius: 30px; height: 30px"
-        placeholder=" &#xf002;" aria-label="Search" />
-</form> --}}
-{{-- @endsection --}}
-<!DOCTYPE html>
-<html lang="en">
-    
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    
-    <title>Prodamas Plus</title>
-    
-    <!--Goofle Font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap"
-        rel="stylesheet" />
-        
-        <!--font awesome-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-        
-        <!--CSS-->
+@section('title', 'Artikel')
+
         @section('head')
         <link rel="stylesheet" href="css/artikel.css">
         @endsection
-</head>
 
-<body>
     @section('content')
     <!-- awal jumbotron -->
     <div class="container-fluid banner">
@@ -109,7 +78,3 @@
     </div>
     </div>
     @endsection
-
-</body>
-
-</html>
