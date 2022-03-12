b7080b414559a697363ce2e0bb55f436769ad1d2
Facronactz <facronactz@gmail.com>
2022-03-12 20:26:39 +0700

    refactor, clean, and format code

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.desain')
-@section('artikel','active')
+@section('artikel', 'active')
 @section('title', 'Artikel')
 
-        @section('head')
-        <link rel="stylesheet" href="css/artikel.css">
-        @endsection
+@section('head')
+    <link rel="stylesheet" href="css/artikel.css">
+@endsection
 
-    @section('content')
+@section('content')
     <!-- awal jumbotron -->
     <div class="container-fluid banner">
         <div class="container banner-content">
@@ -21,23 +21,24 @@
             <!-- Kiri -->
             <div class="col">
 
-                @foreach ($artikel as $item)     
-                <div class="mb-3 kartu">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="{{ asset('articleProd/sampul/'.$item->gambar_sampul) }}" class="img-fluid rounded-start">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <a href="artikel/{{$item->id}}" class="link-dark">
-                                    <h5 class="card-title">{{$item->judul}}</h5>
-                                </a>
-                                <p class="card-text">{{$item->text_sampul}}
-                                </p>
+                @foreach ($artikel as $item)
+                    <div class="mb-3 kartu">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <img src="{{ asset('articleProd/sampul/' . $item->gambar_sampul) }}"
+                                    class="img-fluid rounded-start">
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <a href="artikel/{{ $item->id }}" class="link-dark">
+                                        <h5 class="card-title">{{ $item->judul }}</h5>
+                                    </a>
+                                    <p class="card-text">{{ $item->text_sampul }}
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
                 @endforeach
 
 
@@ -47,28 +48,29 @@
                 <div class="card-right bg-light">
                     <h5 class="card-header">Update Artikel</h5>
                     @foreach ($artikelupdate as $item)
-                        
-                    <div class="col-sm-12">
-                        <div class="caption">
-                            <a href="artikel/{{$item->id}}"><h5>{{$item->judul}}</h5></a>
-                            <div class="row">
-                                <div class="col-xl-3">
-                                    <img src="{{ asset('articleProd/sampul/'.$item->gambar_sampul) }}" width="100%" alt="Cinque Terre">
-                                </div>
-                                <div class="col-sm-9">
-                                    <p>{{$item->text_sampul}}</p>
+                        <div class="col-sm-12">
+                            <div class="caption">
+                                <a href="artikel/{{ $item->id }}">
+                                    <h5>{{ $item->judul }}</h5>
+                                </a>
+                                <div class="row">
+                                    <div class="col-xl-3">
+                                        <img src="{{ asset('articleProd/sampul/' . $item->gambar_sampul) }}" width="100%"
+                                            alt="Cinque Terre">
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <p>{{ $item->text_sampul }}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     @endforeach
                 </div>
                 <div class="card-right bg-light">
                     <h5 class="card-header">Kunjungi Website</h5>
                     <ul class="list-group list-group-flush">
                         <a class="list-group-item" href="https://www.kedirikota.go.id/">Pemerintah Kota Kediri</a>
-                        <a class="list-group-item"
-                            href="https://prodamasplus.kedirikota.go.id/auth/login">Progressio</a>
+                        <a class="list-group-item" href="https://prodamasplus.kedirikota.go.id/auth/login">Progressio</a>
                         <a class="list-group-item"
                             href="https://survey123.arcgis.com/share/932769e15a27418aa9b448b0b1e6b6b6">Bank Sampah</a>
                     </ul>
@@ -77,4 +79,4 @@
         </div>
     </div>
     </div>
-    @endsection
+@endsection
--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.desain')
-@section('audio','active')
+@section('media', 'active')
+@section('audio', 'active')
 @section('title', 'Audio')
 
 @section('head')
@@ -19,62 +20,67 @@
     <div class="row row-cols-1 row-cols-md-3 mb-2 g-4 centerItms feedAudio">
         {{-- Card Audio --}}
         @forelse ($audios as $audio)
-        <div class=" card noBorder cardAudio" style="width: 18rem;">
-            <div class="card h-100 noBorder" data-bs-toggle="modal" data-bs-target="#audioPlayer{{ $audio->id }}">
-                <img src="{{ asset('/audioProd/thumb/'.$audio->gambar_sampul) }}" class="card-img-top d-flex justify-content" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">
-                        <p class=" tittleAudio" href="">
-                            {{$audio->judul}}
-                        </p>
-                    </h5>
+            <div class=" card noBorder cardAudio" style="width: 18rem;">
+                <div class="card h-100 noBorder" data-bs-toggle="modal" data-bs-target="#audioPlayer{{ $audio->id }}">
+                    <img src="{{ asset('/audioProd/thumb/' . $audio->gambar_sampul) }}"
+                        class="card-img-top d-flex justify-content" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <p class=" tittleAudio" href="">
+                                {{ $audio->judul }}
+                            </p>
+                        </h5>
+                    </div>
                 </div>
             </div>
-        </div>
 
-        @foreach ($audios as $audio)    
-        {{-- Card Modal --}}
-        <div class="modal fade" id="audioPlayer{{$audio->id}}" tabindex="-1" aria-labelledby="audioPlayerLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-                <div class="modal-content">
-                    <div class="modal-header ">
-                        <h5 class="modal-title" id="audioPlayerLabel">{{ $audio->judul }}</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row row-cols-1 row-cols-md-3 mb-2 g-4 audioPlayerBox centerItms">
-                            <div class="card" style="
+            @foreach ($audios as $audio)
+                {{-- Card Modal --}}
+                <div class="modal fade" id="audioPlayer{{ $audio->id }}" tabindex="-1"
+                    aria-labelledby="audioPlayerLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                            <div class="modal-header ">
+                                <h5 class="modal-title" id="audioPlayerLabel">{{ $audio->judul }}</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="row row-cols-1 row-cols-md-3 mb-2 g-4 audioPlayerBox centerItms">
+                                    <div class="card" style="
+                                    width: 25rem;
+                                    border: none;
+                                    margin-right:1em;
+                                    ">
+                                        <img src="{{ asset('/audioProd/thumb/' . $audio->gambar_sampul) }}"
+                                            class="d-flex justify-content" style="width: 100%;" alt="...">
+                                        <audio controls style="width: 100%;">
+                                            <source src="{{ asset('/audioProd/fileaudio/' . $audio->konten) }}"
+                                                type="audio/mpeg">
+                                            Your browser does not support the audio element.
+                                        </audio>
+                                    </div>
+                                    <div class="card" style="
                                 width: 25rem;
                                 border: none;
-                                margin-right:1em;
                                 ">
-                                <img src="{{ asset('/audioProd/thumb/'.$audio->gambar_sampul) }}" class="d-flex justify-content" style="width: 100%;" alt="...">
-                                <audio controls style="width: 100%;">
-                                    <source src="{{ asset('/audioProd/fileaudio/'.$audio->konten) }}" type="audio/mpeg">
-                                    Your browser does not support the audio element.
-                                </audio>
+                                        <h1> {{ $audio->judul }}</h1>
+                                        <p>{{ $audio->caption }}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="card" style="
-                            width: 25rem;
-                            border: none;
-                            ">
-                                <h1> {{$audio->judul}}</h1>
-                                <p>{{$audio->caption}}</p>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                             </div>
                         </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
                 </div>
-            </div>
-        </div>
-        <!-- Akhir Card Audio -->
-        @endforeach
+                <!-- Akhir Card Audio -->
+            @endforeach
         @empty
-        <div class="alert alert-success" role="alert">
-            Tidak ada data
-        </div>
+            <div class="alert alert-success" role="alert">
+                Tidak ada data
+            </div>
         @endforelse
     </div>
     <!-- akhir feed audio -->
