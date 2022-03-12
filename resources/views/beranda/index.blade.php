da9a6d9381599bfe3a739364a123870a59dad4ef
Farro Axza <farroaxza@gmail.com>
2022-03-12 18:31:58 +0700

    update titel

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.desain')
 @section('beranda','active')
+@section('title', 'Beranda')
 
 <!-- <form class="d-flex">
   <input class="form-control me-2" type="search" style="font-family: 'Font Awesome 5 Free'; font-weight: 600; border-radius: 30px; height: 30px" placeholder=" &#xf002;" aria-label="Search" />
