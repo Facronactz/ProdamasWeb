5b66b6a6de632403f664df105f36c970c2b18f02
Farro Axza <farroaxza@gmail.com>
2022-03-12 18:35:19 +0700

    update view

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -1,31 +1,7 @@
 @extends('layouts.desain')
 @section('beranda','active')
 @section('title', 'Beranda')
-
-<!-- <form class="d-flex">
-  <input class="form-control me-2" type="search" style="font-family: 'Font Awesome 5 Free'; font-weight: 600; border-radius: 30px; height: 30px" placeholder=" &#xf002;" aria-label="Search" />
-</form> -->
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-  <title>Prodamas Plus</title>
-
-  <!--Goofle Font-->
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap" rel="stylesheet" />
-
-  <!--font awesome-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-
-  <!--CSS-->
-  @section('head')
+@section('head')
   <link rel="stylesheet" href="css/beranda.css">
   <style>
     .carousel-img {
@@ -40,10 +16,7 @@
         max-height: 300px;
     }
   </style>
-  @endsection
-</head>
-
-<body>
+@endsection
 
   @section('content')
   <!-- awal jumbotron -->
@@ -386,6 +359,3 @@
 
 
   @endsection
-</body>
-
-</html>
