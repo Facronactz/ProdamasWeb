0573e93acf6ade471783a3702655468caa1ca14b
Farro Axza <farroaxza@gmail.com>
2022-03-12 18:50:41 +0700

    cleaning code

--- resources/views/artikel/artikelLay.blade.php
+++ resources/views/artikel/artikelLay.blade.php
@@ -1,31 +1,10 @@
 @extends('layouts.desain')
 @section('artikel', 'active')
-<!DOCTYPE html>
-<html lang="en">
-
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        
-        <title>Prodamas Plus</title>
-        
-        <!--Goofle Font-->
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap" rel="stylesheet" />
-        
-        <!--font awesome-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-        
-  <!--CSS-->
+@section('title', 'Artikel')
   @section('head')
   <link rel="stylesheet" href="{{ asset('/css/artikel.css') }}">
   @endsection
-</head>
 
-<body>
-    <!-- Content -->
     @section('content')
     <div class="row">
         <div class="leftcolumn mt-5">
@@ -78,5 +57,3 @@
     </div>
 <!-- Akhir content -->
 @endsection
-</body>
-</html>
--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -1,46 +1,12 @@
 @extends('layouts.desain')
 @section('audio','active')
-{{-- @section('search') --}}
-<form class="d-flex">
-    <input class="form-control me-2" type="search" style="font-family: 'Font Awesome 5 Free'; font-weight: 600; border-radius: 30px; height: 30px" placeholder=" &#xf002;" aria-label="Search" />
-</form>
-{{-- @endsection --}}
-@section('content')
+@section('title', 'Audio')
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-
-    <title>Prodamas Plus</title>
-
-    <!--icon-->
-    <link href="img/logoprodamas.png" rel="icon" />
-
-    <!--Goofle Font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap" rel="stylesheet" />
-
-    <!--font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-
-    <!-- Template Main CSS File
-    <LINK rel="stylesheet" type="text/css"
-     href="media.css" title="Default Styles" media="screen">-->
-
-    <!--CSS-->
+@section('head')
     <link rel="stylesheet" href="css/media.css">
-</head>
+@endsection
 
-<body>
-
+@section('content')
     <!-- awal jumbotron -->
     <div class="banner">
         <video id="background-video" autoplay loop muted poster="kota.png">
@@ -112,16 +78,4 @@
         @endforelse
     </div>
     <!-- akhir feed audio -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-</body>
-
-</html>
 @endsection
