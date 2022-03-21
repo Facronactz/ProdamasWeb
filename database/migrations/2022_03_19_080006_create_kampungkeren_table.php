ca026d9da5632e6579d7acc915c62d86ec04cfad
achieto <anggie.t17210@gmail.com>
2022-03-22 00:08:05 +0700

    CRUD Kampung Keren

---
+++ database/migrations/2022_03_19_080006_create_kampungkeren_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKampungkerenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kampungkeren', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('judul', 100);
+            $table->string('foto');
+            $table->string('caption', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kampungkeren');
+    }
+}
---
+++ resources/views/admin/kampungkeren/list.blade.php
@@ -0,0 +1,58 @@
+@extends('admin.master')
+
+@section('kampungkeren', 'active')
+
+@section('title')
+Kampung Keren Submission
+@endsection
+
+@section('content')
+@if(session('success'))
+<div class="alert alert-success">
+    {{session('success')}}
+</div>
+@endif
+
+<a href="/admin/add-kampungkeren" class="btn btn-primary mb-3">Tambah Kampung Keren</a>
+<table class="table" id="tableFoto">
+    <thead class="thead-light">
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Tanggal</th>
+            <th scope="col">Judul</th>
+            <th scope="col">Foto</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($kampungkeren as $key=>$karen)
+        <tr>
+            <td>{{$key + 1}}</th>
+            <td>{{$karen->created_at}}</td>
+            <td>{{$karen->judul}}</td>
+            <td>{{$karen->foto}}</td>
+            <td>
+                <form action="/admin/kampungkeren/{{$karen->id}}" method="POST">
+                    <a href="/admin/kampungkeren/{{$karen->id}}" class="btn btn-info">Edit</a>
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-danger my-1" onclick="return confirm('Yakin Ingin Menghapus Foto?')" value="Delete">
+                </form>
+            </td>
+        </tr>
+        @empty
+        <tr colspan="3">
+            <td>No data</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+@endsection
+
+@section('table')
+<script>
+    $(document).ready(function() {
+        $('#tableFoto').DataTable(); //sesuikan id tabel yg dibat
+    });
+</script>
+@endsection
