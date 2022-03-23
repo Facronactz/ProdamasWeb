b232d47c35e308bebc1a97bcd356f1a55b95dbe8
achieto <anggie.t17210@gmail.com>
2022-03-24 03:32:44 +0700

    Update CRUD Kampung Keren

--- app/Http/Controllers/KampungkerenAdminController.php
+++ app/Http/Controllers/KampungkerenAdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kampungkeren;
+use App\Models\DescriptionAdmin;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 
@@ -26,7 +27,7 @@ class KampungkerenAdminController extends Controller
         foreach ($request->file('foto') as $file) {
             if ($file->isValid()) {
                 $foto = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $file->getClientOriginalName());
-                $file->move(public_path('../kampungkerenProd/'), $foto);
+                $file->move(public_path('../public/kampungkerenProd/'), $foto);
                 $files[] = [
                     'judul' => $request->judul,
                     'foto' => $foto,
@@ -42,7 +43,8 @@ class KampungkerenAdminController extends Controller
     public function index()
     {
         $kampungkeren = Kampungkeren::groupBy('judul')->get();
-        return view('admin.kampungkeren.list', compact('kampungkeren'));
+        $descriptions = DescriptionAdmin::first()->get();
+        return view('admin.kampungkeren.list', compact('kampungkeren', 'descriptions'));
     }
 
     public function edit($id)
@@ -87,5 +89,25 @@ class KampungkerenAdminController extends Controller
     {
         $submission = DB::table('kampungkeren')->where('id', $id)->delete();
         return redirect('/admin/list-kampungkeren')->with('success', 'Kampung Keren Berhasil Dihapus!');
+    }
+
+    public function edit_desc()
+    {
+        $descriptions = DescriptionAdmin::first();
+        return view('admin.kampungkeren.editdesc', compact('descriptions'));
+    }
+
+    public function update_desc(Request $request)
+    {
+        $request->validate([
+            'desc_kampungkeren' => 'required',
+        ]);
+
+
+        $descriptions = DescriptionAdmin::first();
+        $descriptions_data = ["desc_kampungkeren" => $request["desc_kampungkeren"]];
+        $descriptions->update($descriptions_data);
+
+        return redirect('/admin/list-kampungkeren')->with('success', 'Deskripsi Berhasil Diupdate!');
     }
 }
