fa0748317df67601b0582e1b2110e303bde18dbe
Facronactz <facronactz@gmail.com>
2022-03-25 20:13:31 +0700

    update pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        paginator::defaultView('views.vendor.pagination.bootstrap-4');
+        paginator::defaultView('vendor.pagination.bootstrap-4');
     }
 }
