a3ee4ec72c9954f9fababf01eb3635f25714e009
Facronactz <facronactz@gmail.com>
2022-03-25 22:03:21 +0700

    fix some bug

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -37,7 +37,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>">
+                    </div>
                 @endforeach
                 <div class="d-flex justify-content-end w-100 my-3">
                     {{ $artikel->links() }}
