7bbdc8875105220616503c4fcfe182dab0eeb2e0
achieto <anggie.t17210@gmail.com>
2022-03-29 00:35:17 +0700

    Update Description

--- app/Http/Controllers/KampungkerenController.php
+++ app/Http/Controllers/KampungkerenController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Kampungkeren;
+use App\Models\DescriptionAdmin;
 
 class KampungkerenController extends Controller
 {
     public function index()
     {
+        $descriptions = DescriptionAdmin::first()->get();
         $kampungkeren = Kampungkeren::orderBy('id', 'desc')->simplePaginate(5);
-        return view('kampungkeren.index', compact('kampungkeren'));
+        return view('kampungkeren.index', compact('kampungkeren', 'descriptions'));
     }
 }
