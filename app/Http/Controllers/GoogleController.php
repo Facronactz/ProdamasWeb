766212b4a5efa7e4e36ce9fe674f1eec552bae0a
rizal bayhaqi <rizalbayhaqi73@gmail.com>
2022-04-04 10:40:22 +0700

    jbuu

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 //use App\Http\GoogleController;
 use Iluminated\Support\Facades\Auth;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleController extends Controller
