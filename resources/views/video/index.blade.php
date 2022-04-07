1deb944849a54ef5f3f1331f45f484e68e75e597
Facronactz <facronactz@gmail.com>
2022-04-07 15:54:37 +0700

    updatee yt 3

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -59,8 +59,8 @@
                                         margin-top: 30px;
                                         ">
                             <video width="100%" max-width="850" height="auto" controls>
-                                <source src="{{ $video->konten) }}">
-                                Your browser does not support the video tag.
+                                {{-- <source src="{{ $video->konten) }}">
+                                Your browser does not support the video tag. --}}
                                 <iframe width="560" height="315" src="{{ $video->konten }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </video>
                         </div>
