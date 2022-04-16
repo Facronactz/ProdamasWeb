d5a087e18ecb05bffb5dee29bc6f27e31dfb7603
mobuurhan <1burhanudin23@gmail.com>
2022-04-17 03:26:12 +0700

    audio index 3

--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -60,8 +60,8 @@
                                     ">
                                 
                                 <iframe src="https://drive.google.com/file/d/1KicuhAcRMgnXs2sdcbk1BfZ73kj3_Ycz/preview" width="fixed" height="fixed" allow="autoplay"></iframe>
-                            {{-- <img src="{{ asset('/audioProd/thumb/' . $audio->gambar_sampul) }}" class="d-flex justify-content" style="width: 100%;" alt="...">
-                            <audio controls style="width: 100%;">
+                            <img src="{{ asset('/audioProd/thumb/' . $audio->gambar_sampul) }}" class="d-flex justify-content" style="width: 100%;" alt="...">
+                            {{-- <audio controls style="width: 100%;">
                                 <source src="{{ asset('/audioProd/fileaudio/' . $audio->konten) }}" type="audio/mpeg">
                                 Your browser does not support the audio element.
                             </audio> --}}
