b97cbf2a767afc49a73d42d571fb2396d5445a1b
Facronactz <facronactz@gmail.com>
2022-04-18 00:05:08 +0700

    fix kampung keren

--- resources/views/kampungkeren/index.blade.php
+++ resources/views/kampungkeren/index.blade.php
@@ -23,19 +23,20 @@
         </div>
     </div> --}}
 
-    <div class="d-flex">
-        <div class="row text-center w-100">
-            <div class="w-100">
-                <h1 class="mb-3">Kampung Keren</h1>
-                @foreach ($descriptions ?? '' as $description)
-                    <p> <?= $description->desc_kampungkeren ?></p>
-                @endforeach
+    <div class="container-lg">
+        <div class="d-flex my-4">
+            <div class="row text-center w-100">
+                <div class="w-100">
+                    <h1 class="mb-3">Kampung Keren</h1>
+                    @foreach ($descriptions ?? '' as $description)
+                        <p> <?= $description->desc_kampungkeren ?></p>
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
 
-    <!--Konten Kampung Keren-->
-    {{-- <div class="kampung">
+        <!--Konten Kampung Keren-->
+        {{-- <div class="kampung">
             <div class="konten">
                 <h2 class="h4 font-italic mb-4">{{ $karen->judul }}</h2>
                 <div class="row">
@@ -49,18 +50,17 @@
             </div>
     </div> --}}
 
-    <div class="container-lg">
         <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 text-center">
-        @foreach ($kampungkeren as $karen)
-            <div class="col d-flex" style="flex-direction: column;">
-                <img src="{{ asset('kampungkerenProd/' . $karen->foto) }}" alt="">
-                <div class="d-flex bg-primary justify-content-center" style="min-height:80px; margin: -30px auto; width:180px; background-color:{{ $karen->warna }}">
-                    <h2 class="my-auto">{{ $karen->judul }}</h2>
+            @foreach ($kampungkeren as $karen)
+                <div class="col d-flex" style="flex-direction: column;">
+                    <img src="{{ asset('kampungkerenProd/' . $karen->foto) }}" alt="">
+                    <div class="d-flex bg-primary justify-content-center" style="min-height:80px; margin: -30px auto; width:180px; background-color:{{ $karen->warna }}">
+                        <h2 class="my-auto">{{ $karen->judul }}</h2>
+                    </div>
+                    <div class="mt-5 mb-4">
+                        <p>{{ $karen->caption }}</p>
+                    </div>
                 </div>
-                <div class="mt-5 mb-4">
-                    <p>{{ $karen->caption }}</p>
-                </div>
-            </div>
             @endforeach
         </div>
         <br>
