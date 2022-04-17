2ec65fa599320e2f5254da48c75793b03775e235
mobuurhan <1burhanudin23@gmail.com>
2022-04-17 11:54:16 +0700

    audio index

--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -61,11 +61,11 @@
                                 
                                 
                             <img src="{{ asset('/audioProd/thumb/' . $audio->gambar_sampul) }}" class="d-flex justify-content" style="width: 100%;" alt="...">
-                            <audio controls>
+                            {{-- <audio controls> --}}
                                 <iframe src={{$audio->konten}} width="fixed" height="fixed" allow="autoplay"></iframe>
                                 {{-- <source src="http://docs.google.com/uc?export=open&id=1lhaQyNS3pwc6JWWg8jmOf4eyK2qRR8wi" type="audio/mp3">
                                 <p>Your browser does not support HTML5 audio :(</p> --}}
-                             </audio> 
+                             {{-- </audio>  --}}
                             
                             {{-- <audio controls style="width: 100%;">
                                 <source src="{{ asset('/audioProd/fileaudio/' . $audio->konten) }}" type="audio/mpeg">
