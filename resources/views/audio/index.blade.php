92cdcc4abc3a2877eb78aba484ad315cad440f06
mobuurhan <1burhanudin23@gmail.com>
2022-04-18 13:33:04 +0700

    index audio

--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -89,39 +89,4 @@
     </div>
 </div>
 <!-- akhir feed audio -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-    <script>
-        $(function() {
-            $('.modal').on('hide.bs.modal', function() {
-                $audio = $(this).find('audio');
-                try {
-                    $audio[0].pause();
-                } catch (error) {
-
-                }
-                $video = $(this).find('video');
-                try {
-                    $video[0].pause();
-                } catch (error) {
-
-                }
-            });
-        });
-        $(function() {
-            $('.modal').on('shown.bs.modal', function() {
-                $media = $(this).find('.media');
-                $img = $(this).find('.click');
-                if ($media) {
-                    $img.unbind().click(function() {
-                        // $media[0].pause();
-                        return $media[0].paused ? $media[0].play() : $media[0].pause();
-                    });
-                }
-            });
-        });
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
-
 @endsection 
