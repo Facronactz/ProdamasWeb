070212dca7e939def4f78567c8fe21587b12d79e
mobuurhan <1burhanudin23@gmail.com>
2022-04-18 12:20:03 +0700

    update

--- resources/views/audio/index.blade.php
+++ resources/views/audio/index.blade.php
@@ -60,7 +60,7 @@
                                     ">
                                 
                                 <img src="{{ asset('/audioProd/thumb/' . $audio->gambar_sampul) }}" class="d-flex justify-content" style="width: 100%;" alt="...">
-                                <iframe src="https://drive.google.com/file/d/1KicuhAcRMgnXs2sdcbk1BfZ73kj3_Ycz/preview" width="640" height="480" allow="autoplay"></iframe>
+                                <iframe src="https://drive.google.com/file/d/1KicuhAcRMgnXs2sdcbk1BfZ73kj3_Ycz/preview" width="fixed" height="fixed" allow="autoplay"></iframe>
 
                             {{-- <iframe src="https://drive.google.com/file/d/1KicuhAcRMgnXs2sdcbk1BfZ73kj3_Ycz/preview" width="fixed" height="fixed" allow="autoplay"></iframe> --}}
                             {{-- <audio controls style="width: 100%;">
