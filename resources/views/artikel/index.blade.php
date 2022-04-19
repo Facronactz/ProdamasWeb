98d88f8079222b093ee21a9a78745af8259f8a42
Facronactz <facronactz@gmail.com>
2022-04-19 12:01:17 +0700

    fix link article

--- resources/views/artikel/index.blade.php
+++ resources/views/artikel/index.blade.php
@@ -36,7 +36,7 @@
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <a href="{{url('artikel/{{ $item->id }}')}}" class="stretched-link"></a>
+                                    <a href="artikel/{{ $item->id }}" class="stretched-link"></a>
                                     <h5 class="card-title" style="font-size: 22px;">{{ $item->judul }}</h5>
                                     <p class="card-text" style="font-size: 14px;"><?= $item->text_sampul ?></p>
                                     <p class="card-text" style="font-size: 13px;"><small class="text-muted">Last updated 3 mins ago</small></p>
