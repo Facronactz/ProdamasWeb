a3eab9496a2492ef5e53f36f577a5fa45c0db914
achieto <anggie.t17210@gmail.com>
2022-04-19 09:26:51 +0700

    Prodamas DataTable rev

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -285,6 +285,9 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
         </script>
 
+        @stack('scripts')
+        @yield('table')
+
         {{-- Modal Login --}}
         <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
