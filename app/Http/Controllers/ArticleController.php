8f0c8848b341cd172a3bf2f9a586a5fa29e29076
Facronactz <facronactz@gmail.com>
2022-04-25 15:05:03 +0700

    test tag

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -86,7 +86,7 @@ class ArticleController extends Controller
 
     public function edit($id) {
         $article = ArticleAdmin::findOrFail($id);
-        $artikel = DB::query('select * from tagging_tagged where taggable_id = $id')->get();
+        $artikel = DB::table('tagging_tagged')->where('taggable_id','=', $id)->get();
         echo json_encode($artikel);
         die();
         $tagg = "";
