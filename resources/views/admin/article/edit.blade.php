d91c8e715d226de3683ba71327d1773d71671b6e
Facronactz <facronactz@gmail.com>
2022-04-25 13:50:50 +0700

    test tag

--- resources/views/admin/article/edit.blade.php
+++ resources/views/admin/article/edit.blade.php
@@ -66,10 +66,11 @@
         </div>
         <div class="form-group">
             <label for="tags">Tag</label>
-            {{-- @foreach ($article->tags as $tag)
+            <br>
+            @foreach ($article->tags as $tag)
                 <label class="label label-info">{{ $tag->name }}</label>
-            @endforeach --}}
-            <input class="form-control" type="text" data-role="tagsinput" name="tags" value="{{ $article->tagNames }}"
+            @endforeach
+            <input class="form-control" type="text" data-role="tagsinput" name="tags">
             @error('tags')
                 <div class="alert alert-danger">
                     {{ $message }}
