978dfddfe200473711fc1504d3590ab527109273
Facronactz <facronactz@gmail.com>
2022-04-28 11:52:54 +0700

    test admin menu

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\admin_menu;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        $menus = admin_menu::all();
-        return view('auth.loginuser', compact('menus'));
+        return view('auth.loginuser');
     }
 }
