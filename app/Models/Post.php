8cf0f8ce87dfbfb4032fda69cf32355db13b238d
anggerlissamsudi <anggerlissamsudi06@gmail.com>
2022-05-10 13:32:27 +0700

    foto sudah tinggal front end

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $table = "posts";
+    protected $fillable=[
+        'title',
+        'body',
+        'cover',
+    ];
+
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
+}
