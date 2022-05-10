ebb9b852f085f24068a84b46267afcddfcdb9524
anggerlissamsudi <anggerlissamsudi06@gmail.com>
2022-05-10 15:58:53 +0700

    sudah

--- resources/views/admin/foto/add.blade.php
+++ resources/views/admin/foto/add.blade.php
@@ -3,88 +3,36 @@
 @section('foto', 'active')
 
 @section('title')
-Foto Baru
+    Foto Baru
 @endsection
 
 @section('content')
 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-    <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
-    </symbol>
+  <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+  </symbol>
 </svg>
 
 @if(session('success'))
 <div class="alert alert-success d-flex align-items-center" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
-        <use xlink:href="#check-circle-fill" />
-    </svg>
-    <div>
-        {{session('success')}}
-        <button type="button" class="btn-close t" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
+  <svg class="bi flex-shrink-0 me-2" width="26" height="26" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+  <div>
+    {{session('success')}}
+    <button type="button" class="btn-close t" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>
 </div>
 @endif
 
-<!-- notif
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-        </symbol>
-    </svg>
-    @if(session('success'))
-        <div class="alert alert-success d-flex align-items-center" role="alert">
-        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-            {{session('success')}}
-            <button type="button" class="btn-close t" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif -->
+    <form action="/admin/list-foto" method="POST"  enctype="multipart/form-data" >
+        @csrf
+        <input type="text" name="title" class="form-control m-2" placeholder="Judul Kegiatan">
+        <Textarea name="body" cols="20" rows="4" class="form-control m-2" placeholder="Deskripsi Kegiatan"></Textarea>
+        <label class="m-2">Sampul Foto Kegiatan</label>
+        <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="cover">
 
-<!-- @if(session('success')) 
-        <div class="alert alert-success">
-            {{session('success')}}
-            <button type="button" class="btn-close t" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif -->
-
-<form action="/admin/list-foto" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div class="form-group">
-        <label for="judul">Judul</label>
-        <input name="judul" class="form-control" name="judul" id="judul" placeholder="Judul">
-        @error('judul')
-        <div class="alert alert-danger">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="konten">Konten</label><br>
-        <input type="file" class="form-control" accept="image/png, image/jpg, image/jpeg" name="konten[]" id="konten[]" multiple>
-        @error('konten')
-        <div class="alert alert-danger">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="caption">Caption</label>
-        <textarea name="caption" id="description" class="form-control" cols="30" rows="10"></textarea>
-        @error('caption')
-        <div class="alert alert-danger">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-    <div class="form-group">
-        <label for="tags">Tag</label>
-        <input class="form-control" type="text" data-role="tagsinput" name="tags">
-        @error('tags')
-        <div class="alert alert-danger">
-            {{ $message }}
-        </div>
-        @enderror
-    </div>
-    <button type="submit" class="btn btn-primary">Tambah Foto</button>
-    <a href="/admin/list-foto" class="btn btn-outline-primary">Kembali</a>
-</form>
+        <label class="m-2">Isi Foto Kegiatan (*maksimal 8 foto kegiatan)</label>
+        <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="images[]" multiple>
+        <button type="submit" class="btn btn-primary">Tambah Foto</button>
+        <a href="/admin/list-foto" class="btn btn-outline-primary">Kembali</a>
+    </form>
 @endsection
