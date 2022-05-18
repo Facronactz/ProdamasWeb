6001556d8de7d12c2d0a352bf0cd91180f9fed3c
achieto <anggie.t17210@gmail.com>
2022-05-18 15:25:02 +0700

    add function controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -91,7 +91,7 @@ class SearchController extends Controller
         
         $keyword = $ok['tag'];
     	$artikel = ArticleAdmin::withAnyTag($keyword)->paginate(3);
-        $foto = FotoAdmin::withAnyTag($keyword)->paginate(3);
+        // $foto = FotoAdmin::withAnyTag($keyword)->paginate(3);
         $audio = AudioAdmin::withAnyTag($keyword)->paginate(3);
         $video = VideoAdmin::withAnyTag($keyword)->paginate(3);
         return view('search.search', compact(
