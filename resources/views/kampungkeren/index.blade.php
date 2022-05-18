2beaff7886b7556c6f4c5ec9d45e2bec1707026d
achieto <anggie.t17210@gmail.com>
2022-05-18 14:05:16 +0700

    add kategori kk

--- resources/views/kampungkeren/index.blade.php
+++ resources/views/kampungkeren/index.blade.php
@@ -5,45 +5,50 @@
 @section('title', 'Kampung Keren')
 
 @section('head')
-    <link rel="stylesheet" href="{{ asset('css/kampung.css') }}">
+<link rel="stylesheet" href="{{ asset('css/kampung.css') }}">
 @endsection
 
 @section('content')
-    <div class="container-fluid p-0">
-        <img class="header-kampung-keren" src="{{ asset('img-tentang/header-kampung-keren.png') }}" alt="" srcset="">
-    </div>
-    <!-- akhir jumbotrom -->
+<div class="container-fluid p-0">
+    <img class="header-kampung-keren" src="{{ asset('img-tentang/header-kampung-keren.png') }}" alt="" srcset="">
+</div>
+<!-- akhir jumbotrom -->
 
-    <div class="container-lg">
-        <div class="d-flex my-4">
-            <div class="row text-center w-100">
-                <div class="w-100">
+<div class="container-lg">
+    <div class="d-flex my-4">
+        <div class="row text-center w-100">
+            <div class="w-100">
+                <a href="/tag?tag=Kampung Keren">
                     <h1 class="mb-3">Kampung Keren</h1>
-                    @foreach ($descriptions ?? '' as $description)
-                        <p> <?= $description->desc_kampungkeren ?></p>
-                    @endforeach
-                </div>
+                </a>
+
+                @foreach ($descriptions ?? '' as $description)
+                <p> <?= $description->desc_kampungkeren ?></p>
+                @endforeach
             </div>
         </div>
-
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 text-center">
-            @foreach ($kampungkeren as $karen)
-                <div class="col d-flex" style="flex-direction: column;">
-                    <img src="{{ asset('kampungkerenProd/' . $karen->foto) }}" alt="">
-                    <div class="d-flex justify-content-center" style="min-height:80px; margin: -30px auto; width:180px; background-color:{{ $karen->warna }}; color:white;">
-                        <h2 class="my-auto">{{ $karen->judul }}</h2>
-                    </div>
-                    <div class="mt-5 mb-4">
-                        <p>{{ $karen->caption }}</p>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-        <br>
-        <div style="text-align:center">
-            {{ $kampungkeren->links() }}
-        </div>
     </div>
 
-    <!--Akhir konten kampung keren-->
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 text-center">
+        @foreach ($kampungkeren as $karen)
+        <a href="/tag?tag={{$karen->judul}}">
+            <div class="col d-flex" style="flex-direction: column;">
+                <img src="{{ asset('kampungkerenProd/' . $karen->foto) }}" alt="">
+                <div class="d-flex justify-content-center" style="min-height:80px; margin: -30px auto; width:180px; background-color:{{ $karen->warna }}; color:white;">
+                    <h2 class="my-auto">{{ $karen->judul }}</h2>
+                </div>
+                <div class="mt-5 mb-4">
+                    <p>{{ $karen->caption }}</p>
+                </div>
+            </div>
+        </a>
+        @endforeach
+    </div>
+    <br>
+    <div style="text-align:center">
+        {{ $kampungkeren->links() }}
+    </div>
+</div>
+
+<!--Akhir konten kampung keren-->
 @endsection
