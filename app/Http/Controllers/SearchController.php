74b3f7ec6a82b9d67e47503686f55844dabf50de
achieto <anggie.t17210@gmail.com>
2022-05-18 15:34:00 +0700

    add function controller

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -91,14 +91,14 @@ class SearchController extends Controller
         
         $keyword = $ok['tag'];
     	$artikel = ArticleAdmin::withAnyTag($keyword)->paginate(3);
-        $fotos = FotoAdmin::withAnyTag($keyword)->paginate(3);
-        $audios = AudioAdmin::withAnyTag($keyword)->paginate(3);
-        $videos = VideoAdmin::withAnyTag($keyword)->paginate(3);
-        return view('search.search', compact(
+        $foto = FotoAdmin::withAnyTag($keyword)->paginate(3);
+        $audio = AudioAdmin::withAnyTag($keyword)->paginate(3);
+        $video = VideoAdmin::withAnyTag($keyword)->paginate(3);
+        return view('artikel.artikelsearch', compact(
             'artikel',
-            'fotos',
-            'audios',
-            'videos'
+            'foto',
+            'audio',
+            'video'
         ));
     }
 }
--- resources/views/artikel/artikelsearch.blade.php
+++ resources/views/artikel/artikelsearch.blade.php
@@ -44,6 +44,34 @@
             Tidak ada data
         </div>
         @endif
+
+        @if ($audio->count())
+        @foreach ($audio as $item)
+        <div class="card mb-3 p-0 hvr-sweep-to-left hvr-bob">
+            <div class="row g-0">
+                <div class="col-md-4 my-auto">
+                    <img src="{{ asset('audioProd/thumb/' . $item->gambar_sampul) }}" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <a href="artikel/{{ $item->id }}" class="stretched-link"></a>
+                        <h5 class="card-title" style="font-size: 22px;">{{ $item->judul }}</h5>
+                        <p class="card-text" style="font-size: 13px;"><small class="text-muted">{{$item->updated_at}}</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+        @if ($audio->hasPages())
+        <div class="d-flex justify-content-end w-100 my-3">
+            {{ $artikel->links() }}
+        </div>
+        @endif
+        @else
+        <div class="alert alert-success" role="alert">
+            Tidak ada data
+        </div>
+        @endif
     </div>
 </div>
 </div>
