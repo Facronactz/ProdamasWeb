c84ecfe6fba1392346f87bab52e5fb0d4ef56f4b
achieto <anggie.t17210@gmail.com>
2022-05-19 17:43:32 +0700

    coba icon carousel

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -62,11 +62,11 @@
             </div> --}}
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselUtama" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="carousel-control-prev" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
     <button class="carousel-control-next" type="button" data-bs-target="#carouselUtama" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="carousel-control-next" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
 </div>
