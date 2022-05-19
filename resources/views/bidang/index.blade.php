1e4c2c24f2c7dbfb82dd099008ef5b6d6205f120
achieto <anggie.t17210@gmail.com>
2022-05-19 10:05:21 +0700

    design navbar coba

--- resources/views/bidang/index.blade.php
+++ resources/views/bidang/index.blade.php
@@ -36,33 +36,35 @@
     @foreach ($bidangs as $bidang)
     @if ($bidang->id % 2 != 0)
     <a href="/tag?tag={{ Str::title($bidang->judul) }}" style="text-decoration: none; color:black">
-    <div class="row row-cols-1 row-cols-md-2 box">
-        <h1 class="d-md-none d-block d-lg-none d-xl-none d-xxl-none">{{ $bidang->judul }}</h1>
-        <div class="col">
-            <div class="bidang-img" style="background-image: url('{{ asset('bidangProd/' . $bidang->foto) }}')"></div>
-        </div>
-        <div class="d-flex align-items-center justify-content-center col">
-            <div class="item-title">
-                <h1 class="d-md-block d-none">{{ $bidang->judul }}</h1>
-                <br>
-                <p>{{ $bidang->caption }}</p>
+        <div class="row row-cols-1 row-cols-md-2 box">
+            <h1 class="d-md-none d-block d-lg-none d-xl-none d-xxl-none">{{ $bidang->judul }}</h1>
+            <div class="col">
+                <div class="bidang-img" style="background-image: url('{{ asset('bidangProd/' . $bidang->foto) }}')"></div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center col">
+                <div class="item-title">
+                    <h1 class="d-md-block d-none">{{ $bidang->judul }}</h1>
+                    <br>
+                    <p>{{ $bidang->caption }}</p>
+                </div>
             </div>
         </div>
-    </div>
+    </a>
     @else
-    <div class="row row-cols-1 row-cols-md-2 box">
-        <h1 class="d-md-none d-block d-lg-none d-xl-none d-xxl-none">{{ $bidang->judul }}</h1>
-        <div class="col order-md-2">
-            <div class="bidang-img" style="background-image: url('{{ asset('bidangProd/' . $bidang->foto) }}')"></div>
-        </div>
-        <div class="d-flex align-items-center justify-content-center col order-md-1">
-            <div class="item-title">
-                <h1 class="d-md-block d-none">{{ $bidang->judul }}</h1>
-                <br>
-                <p>{{ $bidang->caption }}</p>
+    <a href="/tag?tag={{ Str::title($bidang->judul) }}" style="text-decoration: none; color:black">
+        <div class="row row-cols-1 row-cols-md-2 box">
+            <h1 class="d-md-none d-block d-lg-none d-xl-none d-xxl-none">{{ $bidang->judul }}</h1>
+            <div class="col order-md-2">
+                <div class="bidang-img" style="background-image: url('{{ asset('bidangProd/' . $bidang->foto) }}')"></div>
+            </div>
+            <div class="d-flex align-items-center justify-content-center col order-md-1">
+                <div class="item-title">
+                    <h1 class="d-md-block d-none">{{ $bidang->judul }}</h1>
+                    <br>
+                    <p>{{ $bidang->caption }}</p>
+                </div>
             </div>
         </div>
-    </div>
     </a>
     @endif
     @if ($loop->last)
