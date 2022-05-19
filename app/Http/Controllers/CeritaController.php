8a79b5603c45f9c198bc1a9b8db790b6198ff245
qurrotulayunina <77313164+qurrotulayunina@users.noreply.github.com>
2022-05-19 16:21:18 +0700

    Update CeritaController.php

--- app/Http/Controllers/CeritaController.php
+++ app/Http/Controllers/CeritaController.php
@@ -6,15 +6,33 @@ use Illuminate\Http\Request;
 use App\Models\DescriptionAdmin;
 use App\Models\Cerita;
 use App\Models\Tulis_cerita;
+use Illuminate\Support\Facades\DB;
 
 class CeritaController extends Controller
 {
     //
     public function index()
     {
+        // total
+        DB::table('counters')->increment('views');
+        $counter = DB::table('counters')->get();
+        
+
+        // code jumlah pengunjung
+        $artikel = DB::table('articles')
+                ->select(DB::raw('views'));
+        $counter = DB::table('counters')
+                ->select(DB::raw('views'));
+        $totalviews = DB::table('tulis_ceritas')
+                ->select(DB::raw('views'))
+                ->unionAll($artikel)
+                ->unionAll($counter)
+                ->sum('views');
+        // end code jumlah pengunjung
+        
         $tulis_ceritas = Cerita::orderBy('id', 'desc')->paginate(3);
         $descriptions = DescriptionAdmin::first()->get();
-        return view('cerita.index', compact('tulis_ceritas', 'descriptions'));
+        return view('cerita.index', compact('tulis_ceritas', 'descriptions', 'totalviews', 'counter'));
     }
     public function show($id)
     {
