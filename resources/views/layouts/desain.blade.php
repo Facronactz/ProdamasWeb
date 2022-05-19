2517ea7ed1388fe74fc00ac9718d0eb1179d659b
achieto <anggie.t17210@gmail.com>
2022-05-19 16:21:18 +0700

    change navbar

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -171,7 +171,7 @@
                                                 <form action="search">
                                                     <div style="display:flex; line-height:30px; align-items: center; vertical-align: middle;">
                                                         <div>
-                                                            <input type="text" placeholder="Search" name="search" value="{{ request('search') }}" aria-label="Search">
+                                                            <input type="text" placeholder="Search" name="search" value="{{ request('search') }}" aria-label="Search" style="border: 1px solid rgba(0, 0, 0, 0.31); border-radius: 15px;">
                                                         </div>
                                                         <button class="btn-search" type="submit" style="color:white">
                                                             <i class="fas fa-arrow-right"></i>
