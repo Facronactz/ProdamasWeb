ec07c698b8b2d605bfacd6833305c1e7e56392d0
qurrotulayunina <77313164+qurrotulayunina@users.noreply.github.com>
2022-05-19 23:27:53 +0700

    Update index.blade.php

--- resources/views/foto/index.blade.php
+++ resources/views/foto/index.blade.php
@@ -4,34 +4,8 @@
 
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-
-    <title>Prodamas Plus</title>
-
-    <!--Goofle Font-->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Montserrat&display=swap" rel="stylesheet" />
-
-    <!--font awesome-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> -->
-
-    <!--CSS-->
-    <link rel="stylesheet" href="css/media.css">
-</head>
 
 <body>
-
     <!-- corousel -->
     
     <div class="row row-cols-1 row-cols-md-3 mb-5 g-4 centerItms feedAudio">
@@ -103,6 +77,9 @@
         </div>
         {{-- Akhir Card foto --}}
         @endforeach
+        <div class="d-flex justify-content-end w-100 mb-4">
+            {{ $posts->links() }}
+        </div>
     <!-- corousel -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
