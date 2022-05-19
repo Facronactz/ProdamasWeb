169f495ecbe643ac8bf170cf5a9db8a9f6c3bebc
achieto <anggie.t17210@gmail.com>
2022-05-19 14:11:24 +0700

    design navbar coba

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -109,9 +109,9 @@
                     </button>
 
                     <!-- Content -->
-                    <div class="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
+                    <div class="collapse navbar-collapse justify-content-between" style="border-bottom: 7px solid #FF6600 ;" id=" navbarNavAltMarkup">
                         <!-- Middle -->
-                        <div class="navbar-nav" style="border-bottom: 7px solid #FF6600 ;">
+                        <div class="navbar-nav">
                             <a class="nav-link nav-list @yield('beranda')" aria-current="page" href="{{ url('/') }}"><img src="{{asset('img/ICON HOME.png')}}" style="width: 20px; height: 20px; margin-right: 1rem"></a>
                             <a class="nav-link nav-list @yield('artikel')" href="{{ url('/artikel') }}">Artikel</a>
                             <div class="nav-item dropdown nav-list">
