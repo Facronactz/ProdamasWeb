895ee10ddd1801ba212b27f141e336d03d98b4cc
mobuurhan <1burhanudin23@gmail.com>
2022-05-19 23:14:24 +0700

    beranda foto

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -337,6 +337,7 @@
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
+                                <h5>{{ $post->body }}</h5>
                             </div>
                     </div>
             </div>
