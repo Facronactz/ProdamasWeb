fd0d7d6937a511c9f1b4027bba7a568ac617248d
qurrotulayunina <77313164+qurrotulayunina@users.noreply.github.com>
2022-05-19 18:41:47 +0700

    Update PostFotoController.php

--- app/Http/Controllers/PostFotoController.php
+++ app/Http/Controllers/PostFotoController.php
@@ -5,6 +5,7 @@ use App\Models\Post;
 use App\Models\Image;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostFotoController extends Controller
 {
