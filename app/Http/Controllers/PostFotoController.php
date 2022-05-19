eb80baa746cb5d85d10e2d3f2b1c46d88a8c630a
Facronactz <facronactz@gmail.com>
2022-05-19 18:33:42 +0700

    merge foto

---
+++ app/Http/Controllers/PostFotoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+use App\Models\Image;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostFotoController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'desc')->paginate(4);;
+        // $posts = Post::join('images', 'posts.id', '=', 'images.post_id')
+        // ->get(['posts.*', 'images.*']);
+        // echo json_encode($posts);die();
+        foreach($posts as $post){
+            $post['image'] = Image::where('post_id', $post->id)
+            ->get();
+        }
+        
+        // echo json_encode($posts);die();
+        return view('foto.index', compact('posts'));
+    }
+    public function show($id)
+    {
+        Post::find($id)->increment('views');
+        $Posts = Post::where('id', $id)
+                ->orderBy('id', 'desc')
+                ->get();
+        // dd($tulis_ceritas);
+        return view('foto.index', compact('posts'));
+    }
+}
