8d93143308b918af896ebe5b2374ea524d46e996
mobuurhan <1burhanudin23@gmail.com>
2022-05-19 16:24:03 +0700

    up FOOTER media dan pokmas

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -7,6 +7,20 @@ use Illuminate\Http\Request;
 class MediaController extends Controller
 {
     public function index(){
-        return view('media.index');
+        DB::table('counters')->increment('views');
+        $counter = DB::table('counters')->get();
+        
+
+        // code jumlah pengunjung
+        $artikel = DB::table('articles')
+                ->select(DB::raw('views'));
+        $counter = DB::table('counters')
+                ->select(DB::raw('views'));
+        $totalviews = DB::table('tulis_ceritas')
+                ->select(DB::raw('views'))
+                ->unionAll($artikel)
+                ->unionAll($counter)
+                ->sum('views');
+        return view('media.index', compact ('totalviews','counter'));
     }
 }
