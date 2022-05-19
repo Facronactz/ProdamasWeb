fb5cb81f36d3399c7e81822394fa65f1bbfb722e
agatabimaa <agatabima01@gmail.com>
2022-05-19 13:19:40 +0700

    Paginasi cerita

--- resources/views/cerita/index.blade.php
+++ resources/views/cerita/index.blade.php
@@ -66,6 +66,9 @@
                         </div>
                     </div>
                 @endforeach
+                <div class="d-flex justify-content-end w-100 mb-4">
+                    {{ $tulis_ceritas->links() }}
+                </div>
             </div>
 
 
