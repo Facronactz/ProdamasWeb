6bb40caae2bf4ab9716aba14b5c2534c7acb37df
agatabimaa <agatabima01@gmail.com>
2022-05-19 23:12:46 +0700

    32

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -314,21 +314,31 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                <div class="row row-cols-1 row-cols-md-2 mb-2 g-4 videoPlayerBox centerItms">
-                    <div class="card" style="
-                                  border: none;
-                                  margin: 0;
-                                  margin-top: 30px;
-                                  ">
-                        <img class="d-flex" src="{{ asset('cover/' . $post->cover) }}" controls>
+                <div class="card-body">
+                    <div id="carouselIndicators{{ $post->id }}" class="carousel slide" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    @foreach ($post->images as $no => $images)
+                                        @if ($no == 0)
+                                            <div class="carousel-item active">
+                                                <img src="{{ asset('images/' . $images->image) }}" class="d-block w-100" alt="...">
+                                            </div>
+                                        @else
+                                            <div class="carousel-item">
+                                                <img src="{{ asset('images/' . $images->image) }}" class="d-block w-100" alt="...">
+                                            </div>
+                                        @endif
+                                    @endforeach
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators{{ $post->id }}" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators{{ $post->id }}" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
                     </div>
-                    <div class="card" style="
-                                  border: none;
-                                  ">
-                        <h3><?= $post->title ?></h3>
-                        <p><?= $post->body ?></p>
-                    </div>
-                </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
