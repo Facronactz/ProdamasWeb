383d379d6dd7c6be7a7a98888c72b75c9787a2eb
achieto <anggie.t17210@gmail.com>
2022-05-21 14:37:01 +0700

    tes change iframe

--- resources/views/data/progresio.blade.php
+++ resources/views/data/progresio.blade.php
@@ -4,5 +4,5 @@
 @section('title', 'Progressio')
 
 @section('content')
-    <iframe src="https://prodamasplus.kedirikota.go.id/auth/login" style="border: none;width: 90%;height: 90vh;" class="mx-auto d-flex"></iframe>
+    <object data="https://prodamasplus.kedirikota.go.id/auth/login" style="border: none;width: 90%;height: 90vh;" class="mx-auto d-flex"></object>
 @endsection
