acb2a281b804fb2a73107fb6d8b0c22aec17cfb7
Facronactz <facronactz@gmail.com>
2022-05-31 13:18:56 +0700

    add submenu kube

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -216,8 +216,8 @@
                                     <li class="w-100"><a class="dropdown-item @yield('pokmas')" href="{{ url('/pokmas') }}">POKMAS</a></li>
                                     <li class="w-100"><a class="dropdown-item @yield('sosmed')" href="{{ url('/media') }}">MedSos</a></li>
                                     {{-- <li class="w-100"><a class="dropdown-item @yield('eko')" href="{{ url('/kubperasi') }}">Kube & Koperasi</a></li> --}}
-                                    <li class="btn-group dropend w-100">
-                                        <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <li class="btn-group dropdown w-100">
+                                        <button type="button" class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                                           Kube & Koperasi
                                         </button>
                                         <ul class="dropdown-menu">
