7efd90667373e2dd7a3223df00e80a91c9a127b5
Facronactz <facronactz@gmail.com>
2022-05-31 13:39:02 +0700

    test progressio

--- resources/views/data/progresio.blade.php
+++ resources/views/data/progresio.blade.php
@@ -6,3 +6,18 @@
 @section('content')
     <iframe src="https://prodamasplus.kedirikota.go.id/auth/login" style="border: none;width: 90%;height: 90vh;" class="mx-auto d-flex"></iframe>
 @endsection
+
+@section('script')
+
+    {{-- <script type="text/javascript">
+        document.getElementById("logingoogle").href = "https://prodamasplus.kedirikota.go.id/auth/login_google";
+    </script> --}}
+    <script type="text/javascript">
+        document.getElementById("logingoogle").onclick = function() {
+        //   document.getElementById("logingoogle").href="https://prodamasplus.kedirikota.go.id/auth/login_google";
+          window.open("https://prodamasplus.kedirikota.go.id/auth/login_google", '_blank').focus(); 
+          return true;
+        };
+      </script>
+
+@endsection
