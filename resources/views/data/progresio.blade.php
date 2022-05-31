3b1ef6adce00db1b0b624dacdcf7016f7c93ec9d
Facronactz <facronactz@gmail.com>
2022-05-31 14:01:45 +0700

    test progressio

--- resources/views/data/progresio.blade.php
+++ resources/views/data/progresio.blade.php
@@ -12,13 +12,32 @@
     {{-- <script type="text/javascript">
         document.getElementById("logingoogle").href = "https://prodamasplus.kedirikota.go.id/auth/login_google";
     </script> --}}
-    <script type="text/javascript">
+
+    {{-- <script type="text/javascript">
         document.getElementById("logingoogle").onclick = setTimeout(function() {
             //   document.getElementById("logingoogle").href="https://prodamasplus.kedirikota.go.id/auth/login_google";
             //   window.open("https://prodamasplus.kedirikota.go.id/auth/login_google", '_blank').focus();
             window.open("https://prodamasplus.kedirikota.go.id/auth/login_google", 'newwindow', 'width=500,height=500');
             return true;
         }, 5000);
+    </script> --}}
+
+    <script type="text/javascript">
+        function addEvent(element, evnt, funct) {
+            if (element.attachEvent)
+                return element.attachEvent('on' + evnt, funct);
+            else
+                return element.addEventListener(evnt, funct, false);
+        }
+
+        // example
+        addEvent(
+            document.getElementById('logingoogle'),
+            'click',
+            function() {
+                window.open("https://prodamasplus.kedirikota.go.id/auth/login_google", 'newwindow', 'width=500,height=500');
+            }
+        );
     </script>
 
 @endsection
