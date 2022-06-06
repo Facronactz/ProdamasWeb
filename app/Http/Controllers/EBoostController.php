1f4528430b2718f7fa08b6311050ea5e422a4c38
Facronactz <facronactz@gmail.com>
2022-06-06 21:12:46 +0700

    fix eboost

--- app/Http/Controllers/EBoostController.php
+++ app/Http/Controllers/EBoostController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
  
 use App\Http\Controllers\Controller;
  
-class UserController extends Controller
+class EBoostController extends Controller
 {
     public function index()
     {
