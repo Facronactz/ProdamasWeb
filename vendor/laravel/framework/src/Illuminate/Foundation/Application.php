d91ab8556db1a71b62e6200486e63dcb6a95b8b1
Facronactz <facronactz@gmail.com>
2022-06-06 21:02:27 +0700

    fix eboost

--- vendor/laravel/framework/src/Illuminate/Foundation/Application.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Application.php
@@ -25,6 +25,7 @@ use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Conner\Tagging\Providers\TaggingServiceProvider;
 
 class Application extends Container implements ApplicationContract, CachesConfiguration, CachesRoutes, HttpKernelInterface
 {
