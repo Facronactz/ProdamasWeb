f569e19551de907ddff7e99258863372007da0bd
Facronactz <facronactz@gmail.com>
2022-06-06 21:13:53 +0700

    fix eboost

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -316,7 +316,7 @@
                 <div class="d-flex justify-content-end mt-2">
                     <div style="margin-top: 15px; border-radius: 15px; color: white; text-align:center; background-color: #F58634; padding:5px">
                         <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                        <a style="font-size:15px">Kunjungan: {{$totalviews}}</a>
+                        <a style="font-size:15px">Kunjungan: {{ $totalviews ?? '' }}</a>
                         <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
                     </div>
                 </div>
