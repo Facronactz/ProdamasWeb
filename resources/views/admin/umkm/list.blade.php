3016445def6599592fdcfb56ff7236fc96972987
achieto <anggie.t17210@gmail.com>
2022-06-07 15:44:24 +0700

    CRUD UMKM 3

--- resources/views/admin/umkm/list.blade.php
+++ resources/views/admin/umkm/list.blade.php
@@ -40,39 +40,6 @@ UMKM Submission
         </tbody>
     </table>
 </div>
-
-<!-- <a href="/admin/add-pokmas" class="btn btn-primary mb-3">Tambah Pokmas</a>
-<table class="table" id="tableFoto">
-    <thead class="thead-light">
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Tanggal</th>
-            <th scope="col">Foto</th>
-            <th scope="col">Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($pokmass as $key=>$pokmas)
-        <tr>
-            <td>{{$key + 1}}</th>
-            <td>{{$pokmas->created_at}}</td>
-            <td>{{$pokmas->foto}}</td>
-            <td>
-                <form action="/admin/pokmas/{{$pokmas->id}}" method="POST">
-                    <a href="/admin/pokmas/{{$pokmas->id}}" class="btn btn-info">Edit</a>
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" class="btn btn-danger my-1" onclick="return confirm('Yakin Ingin Menghapus Foto?')" value="Delete">
-                </form>
-            </td>
-        </tr>
-        @empty
-        <tr colspan="3">
-            <td>No data</td>
-        </tr>
-        @endforelse
-    </tbody>
-</table> -->
 @endsection
 
 @section('table')
