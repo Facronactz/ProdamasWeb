484d5578a93d3e0240a5c29ad0a17d4d6fc00fcd
mobuurhan <1burhanudin23@gmail.com>
2022-06-07 14:33:17 +0700

    eboostupdate

--- app/Http/Controllers/EboostAdmin.php
+++ app/Http/Controllers/EboostAdmin.php
@@ -3,8 +3,108 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Eboost;
+use App\Models\DescriptionAdmin;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 class EboostAdmin extends Controller
 {
-    
+    public function create()
+    {
+        return view('admin.bidang.add');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'judul' => 'required',
+            'foto' => 'required',
+            'caption' => 'required',
+        ]);
+
+        $files = [];
+        foreach ($request->file('foto') as $file) {
+            if ($file->isValid()) {
+                $foto = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $file->getClientOriginalName());
+                $file->move(public_path('../bidangProd/'), $foto);
+                $files[] = [
+                    'judul' => strtoupper($request->judul),
+                    'foto' => $foto,
+                    'caption' => $request->caption
+                ];
+            }
+        }
+        Eboost::insert($files);
+
+        return redirect('/admin/list-bidang')->with('success', 'Bidang Berhasil Ditambahkan!');
+    }
+
+    public function index()
+    {
+        $Eboost = Eboost::groupBy('judul')->get();
+        $descriptions = DescriptionAdmin::first()->get();
+        return view('admin.bidang.list', compact('bidangs', 'descriptions'));
+    }
+
+    public function edit($id)
+    {
+        $bidang = Eboost::findOrFail($id);
+        return view('admin.bidang.edit', compact('bidang'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'judul' => 'required',
+            'foto' => 'mimes:jpeg,jpg,png|max:2200',
+            'caption' => 'required'
+        ]);
+
+        $bidangs = Eboost::findorfail($id);
+        $file = $request->file('foto');
+        if ($file != NULL) {
+            File::delete(public_path("../bidangProd/" . $bidangs->foto));
+            $filePath = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $file->getClientOriginalName());
+            $file->move(public_path('../bidangProd/'), $filePath);
+
+            $bidangs->update([
+                'judul' => $request->judul,
+                'foto' => $filePath,
+                'caption' => $request->caption
+            ]);
+        } else {
+            $bidangs->update([
+                'judul' => $request->judul,
+                'caption' => $request->caption
+            ]);
+        }
+
+        return redirect('/admin/list-bidang')->with('success', 'Bidang Berhasil Diupdate!');
+    }
+
+    public function destroy($id)
+    {
+        $submission = DB::table('eboost')->where('id', $id)->delete();
+        return redirect('/admin/list-bidang')->with('success', 'Bidang Berhasil Dihapus!');
+    }
+
+    public function edit_desc($id)
+    {
+        $descriptions = DescriptionAdmin::findOrFail($id);
+        return view('admin.bidang.editdesc', compact('descriptions'));
+    }
+
+    public function update_desc($id, Request $request)
+    {
+        $request->validate([
+            'desc_bidang' => 'required',
+        ]);
+
+        $descriptions = DescriptionAdmin::findOrFail($id);
+        $descriptions_data = ["desc_bidang" => $request["desc_bidang"]];
+        $descriptions->update($descriptions_data);
+
+        return redirect('/admin/list-bidang')->with('success', 'Deskripsi Berhasil Diupdate!');
+    }
 }
+
