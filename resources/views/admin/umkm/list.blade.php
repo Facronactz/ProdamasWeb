9d7bacdb93a0e740351dda30d6374ce92d64c9ad
achieto <anggie.t17210@gmail.com>
2022-06-07 17:32:04 +0700

    CRUD UMKM 11

--- resources/views/admin/umkm/list.blade.php
+++ resources/views/admin/umkm/list.blade.php
@@ -47,6 +47,7 @@ UMKM Submission
             <th scope="col">ID</th>
             <th scope="col">Judul</th>
             <th scope="col">Kelurahan</th>
+            <th scope="col">Jenis</th>
             <th scope="col">Aksi</th>
         </tr>
     </thead>
@@ -56,6 +57,7 @@ UMKM Submission
             <td>{{$key + 1}}</th>
             <td>{{$umkm->judul}}</td>
             <td>{{$umkm->kelurahan}}</td>
+            <td>{{$umkm->jenis}}</td>
             <td>
                 <form action="/admin/umkm/{{$umkm->id}}" method="POST">
                     <a href="/admin/umkm/{{$umkm->id}}" class="btn btn-info">Edit</a>
