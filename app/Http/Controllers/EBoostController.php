1dccdb5eb0ed44a397e839448b346f9b6fb3f6c2
achieto <anggie.t17210@gmail.com>
2022-06-07 09:48:07 +0700

    total view eboost

--- app/Http/Controllers/EBoostController.php
+++ app/Http/Controllers/EBoostController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
  
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
  
 class EBoostController extends Controller
 {
     public function index()
     {
-        return view('eboost.index');
+        DB::table('counters')->increment('views');
+        $counter = DB::table('counters')
+        ->select(DB::raw('views'));
+        $artikel = DB::table('articles')
+        ->select(DB::raw('views'));
+        $totalviews = DB::table('tulis_ceritas')
+        ->select(DB::raw('views'))
+            ->unionAll($artikel)
+            ->unionAll($counter)
+            ->sum('views');
+        return view('eboost.index', compact('counter', 'totalviews'));
     }
 
     public function umkm(){
