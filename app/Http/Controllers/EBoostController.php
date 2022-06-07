9a11dc11ecef5a364e37fed2337de67ba020c632
mobuurhan <1burhanudin23@gmail.com>
2022-06-07 09:31:30 +0700

    ControllerviewEBOOST

--- app/Http/Controllers/EBoostController.php
+++ app/Http/Controllers/EBoostController.php
@@ -14,4 +14,16 @@ class EBoostController extends Controller
     public function umkm(){
         return view('eboost.umkm');
     }
+    public function kube(){
+        return view('eboost.kube');
+    }
+    public function koperasirw(){
+        return view('koperasirw.kube');
+    }
+    public function aksespermodalan(){
+        return view('aksespermodalan.kube');
+    }
+    public function publikasi(){
+        return view('publikasi.kube');
+    }
 }
