17909e7cb1b062882b04995607c0065eada06034
achieto <anggie.t17210@gmail.com>
2022-06-09 14:40:00 +0700

    Paginasi

--- resources/views/foto/index.blade.php
+++ resources/views/foto/index.blade.php
@@ -23,9 +23,9 @@
         </form> -->
 </div>
 <div class="container">
-    <div class="row row-cols-1 row-cols-md-3 mb-5 gx-4 centerItms feedAudio">
+    <div class="row row-cols-1 row-cols-md-3 mb-5 gx-4 centerItms ">
         @forelse ($posts as $post)
-        <div class=" card noBorder cardAudio p-0" style="margin: 5px 5px 5px 0">
+        <div class=" card noBorder cardAudio p-0">
             <div class="card h-100 noBorder" data-bs-toggle="modal" data-bs-target="#audioPlayer{{$post->id}}">
                 <img src="{{ asset('cover/'. $post->cover) }}" style="width: 100%; height: 270px; object-fit: cover; object-position: center" class="card-img-top d-flex justify-content" alt="foto-prodamas">
                 <div class="card-body">
