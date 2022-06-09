8c7632ee0b5ed110a70a630376bee7b231a62106
achieto <anggie.t17210@gmail.com>
2022-06-09 13:02:35 +0700

    CRUD Menu 9

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;\
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\CustomAuthController;
 use App\Http\Controllers\AuthController;
