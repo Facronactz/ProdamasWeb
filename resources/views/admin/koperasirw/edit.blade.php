af855b5f5b977114c8a9a52e79700c375e481336
qurrotulayunina <qanina00@gmail.com>
2022-06-09 13:49:02 +0700

    edit foto koperasi

--- resources/views/admin/koperasirw/edit.blade.php
+++ resources/views/admin/koperasirw/edit.blade.php
@@ -20,6 +20,16 @@ Edit Informasi Koperasi RW
         <label for="informasi">Informasi Koperasi</label>
         <textarea name="informasi" id="description" class="form-control" cols="30" rows="3"><?= $koperasi->informasi ?></textarea>
     </div>
+    <div class="form-group">
+        <label for="foto">Syarat</label><br>
+        <p>{{$koperasi->foto_syarat}}</p>
+        <input type="file" class="form-control" accept="image/png, image/jpg, image/jpeg" name="foto_syarat" id="foto_syarat" value="{{$koperasi->foto_syarat}}">
+        @error('foto')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
     <button type="submit" class="btn btn-primary">Edit</button>
     <a href="/admin/list-koperasirw" class="btn btn-outline-primary">Back</a>
 </form>
