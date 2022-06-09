8dfee335e7fac42c183c185e2b310d6e43a941cb
mobuurhan <1burhanudin23@gmail.com>
2022-06-09 14:51:30 +0700

    KURNIAview

---
+++ app/Http/Controllers/KurniaController.php
@@ -0,0 +1,27 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use App\Models\Menu;
+ 
+class EBoostController extends Controller
+{
+public function index()
+    {
+        DB::table('counters')->increment('views');
+        $counter = DB::table('counters')
+        ->select(DB::raw('views'));
+        $artikel = DB::table('articles')
+        ->select(DB::raw('views'));
+        $totalviews = DB::table('tulis_ceritas')
+        ->select(DB::raw('views'))
+            ->unionAll($artikel)
+            ->unionAll($counter)
+            ->sum('views');
+        $menus = Menu::where('status', 'Show')->get();
+        return view('kurnia.index', compact('counter','totalviews', 'menus'));
+    }
+}
