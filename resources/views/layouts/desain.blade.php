2ce5b534d33e6f39eea095a4b4785b025639e458
Facronactz <facronactz@gmail.com>
2022-06-09 08:10:28 +0700

    fix progressio

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -207,13 +207,7 @@
                                     <li class="w-100"><a class="dropdown-item @yield('peta')" href="{{ url('/peta') }}">Prodamas Dalam Peta</a></li>
                                     <li class="w-100"><a class="dropdown-item @yield('banksampah')" href="{{ url('/banksampah') }}">E-Bank Sampah</a></li>
                                     {{-- <li class="w-100"><a class="dropdown-item @yield('progressio')" href="{{ url('/progressio') }}">Progressio</a></li> --}}
-                                    <li class="dropdown-submenu w-100">
-                                        <a class="dropdown-item @yield('progressio')" href="{{ url('/progressio') }}">Progressio <span class="caret fas fa-caret-right"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li class="w-100"><a class="dropdown-item @yield('sosmed')" href="{{ url('/media') }}">Login KK</a></li>
-                                            <li class="w-100"><a class="dropdown-item @yield('sosmed')" href="{{ url('/media') }}">Login Google</a></li>
-                                        </ul>
-                                    </li>
+                                    <a class="@yield('progressio')" href="https://progressio.kedirikota.go.id/auth/login">Progressio <span class="caret fas fa-caret-right"></span></a>
                                 </ul>
                             </div>
                             <div class="nav-item dropdown nav-list">
