afc46474240ff0131af395ce18359e5523630292
achieto <anggie.t17210@gmail.com>
2022-06-10 16:15:42 +0700

    Try Change Position

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -289,9 +289,7 @@
                             @endforeach
                             <div class="nav-item dropdown nav-list">
                                 <a class="nav-link dropdown-toggle dropdown-toggle-split @yield('main-tentang')" id="navbarDropdownMenuLinkAbout" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">E-Boost </a>
-                                <li class="dropdown-submenu w-100">
-                                    <a class="dropdown-item @yield('e-boost')" href="{{ url('/e-boost') }}">E-Boost<span class="caret fas fa-caret-right ms-3"></span></a>
-                                </li>
+                                
                                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuEboost">
                                     <li class="dropdown-submenu w-100">
                                         <a class="dropdown-item @yield('sosmed')" href="{{ url('/umkm') }}">Pelaku Usaha <span class="caret fas fa-caret-right ms-3"></span></a>
