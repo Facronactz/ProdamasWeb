c1147371066b26bec2c5cdad689c4275ff414dbc
reoaldoo <reoaldoandanuwari@gmail.com>
2022-06-13 14:29:12 +0700

    admin wirausaha

---
+++ resources/views/admin/wirausaha/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.master')
+@section('eboost', 'active')
+@section('title')
+Edit E-Boost
+@endsection
+@section('content')
+
+<form action="/admin/edit-eboost/{{$eboost->id}}" method="POST" enctype="application/x-www-form-urlencoded">
+@csrf
+@method('put')
+    <div class="form-group">
+        <label for="judul_tentang">Judul Tentang</label>
+        <input name="judul_tentang" class="form-control" name="judul_tentang" id="judul_tentang" placeholder="Judul Tentang" value="{{ $eboost->judul_tentang }}">
+        @error('judul_tentang')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <label for="caption_tentang">Caption Tentang</label>
+        <textarea name="caption_tentang" class="form-control" name="caption_tentang" id="caption_tentang" placeholder="Caption Tentang">{{ $eboost->caption_tentang }}</textarea>
+        @error('caption_tentang')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <label for="foto_tentang">Foto Tentang</label><br>
+        <p>{{$eboost->foto_tentang}}</p>
+        <input type="file" class="form-control" name="foto_tentang" id="foto_tentang">
+        @error('foto_tentang')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+    
+    <div class="form-group">
+        <label for="judul_info">Judul Info</label>
+        <input name="judul_info" class="form-control" name="judul_info" id="judul_info" placeholder="Judul info" value="{{ $eboost->judul_info }}">
+        @error('judul_info')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <label for="caption_info">Caption Info</label>
+        <textarea name="caption_info" class="form-control" name="caption_info" id="caption info" placeholder="Caption Info">{{ $eboost->caption_info }}</Textarea>
+        @error('caption_info')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <div class="form-group">
+        <label for="foto_info">Foto Info</label><br>
+        <p>{{$eboost->foto_info}}</p>
+        <input type="file" class="form-control" name="foto_info" id="foto_info">
+        @error('foto_info')
+        <div class="alert alert-danger">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+
+    <button type="submit" class="btn btn-primary">Edit</button>
+    <a href="/admin/list-eboost" class="btn btn-outline-primary">Kembali</a>
+</form>
+@endsection
---
+++ resources/views/admin/wirausaha/list.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.master')
+@section('eboost', 'active')
+@section('title')
+E-Boost Submission
+@endsection
+
+@section('content')
+@if(session('success'))
+<div class="alert alert-success">
+    {{session('success')}}
+</div>
+@endif
+
+<div class="form-group">
+    <label for="desc_eboost">Deskripsi</label>
+    <!-- <table class="table">
+        @foreach ($eboost ?? '' as $eboosts)
+        <tr>
+            <td><?= $eboosts->desc_eboost ?></td>
+            <td align="right">
+                <form action="/admin/eboost/{{ $eboosts->id }}" method="POST">
+                    <a href="/admin/eboost/{{ $eboosts->id }}" class="btn btn-info">Edit</a>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table> -->
+</div>
+<a href="/admin/add-eboost" class="btn btn-primary mb-3">Tambah E-Boost</a>
+<table class="table" id="tableEboost">
+    <thead class="thead-light">
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Judul Tentang</th>
+            <th scope="col">Caption Tentang</th>
+            <th scope="col">Foto Tentang</th>
+            <th scope="col">Judul Info</th>
+            <th scope="col">Caption Info</th>
+            <th scope="col">Foto Info</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse ($eboost as $key=>$eboosts)
+        <tr>
+            <td>{{$key + 1}}</th>
+            <td>{{$eboosts->judul_tentang}}</td>
+            <td>{{$eboosts->caption_tentang}}</td>
+            <td>{{$eboosts->foto_tentang}}</td>
+            <td>{{$eboosts->judul_info}}</td>
+            <td>{{$eboosts->caption_info}}</td>
+            <td>{{$eboosts->foto_info}}</td>
+            <td>
+                <form action="/admin/edit-eboost/{{$eboosts->id}}" method="POST">
+                    <a href="/admin/edit-eboost/{{$eboosts->id}}" class="btn btn-info">Edit</a>
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-danger my-1" onclick="return confirm('Yakin Ingin Menghapus EBoost?')" value="Delete">
+                </form>
+            </td>
+        </tr>
+        @empty
+        <tr colspan="3">
+            <td>No data</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
+@endsection
+
+@section('table')
+<script>
+    $(document).ready(function() {
+        $('#tableEboost').DataTable(); //sesuikan id tabel yg dibat
+    });
+</script>
+@endsection
