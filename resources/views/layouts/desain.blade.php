93422cdefa8a5cdaadc812c50dd6257bb4b47b21
achieto <anggie.t17210@gmail.com>
2022-06-14 00:37:00 +0700

    Artikel Pemasaran

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -271,7 +271,7 @@
                                     </li>
                                     </li>
                                     <li class="dropdown-submenu w-100">
-                                    <li class="w-100"><a class="dropdown-item @yield('sosmed')" href="{{ url/search?search=eboost') }}">Pemasaran<span class="caret fas fa-caret-right ms-3"></span></a>
+                                    <li class="w-100"><a class="dropdown-item @yield('sosmed')">Pemasaran<span class="caret fas fa-caret-right ms-3"></span></a>
                                         <ul class="dropdown-menu">
                                             <li class="w-100"><a class="dropdown-item @yield('sosmed')" href="{{ url('/tags?tags=pemasaran') }}">Artikel</a></li>
                                         </ul>
