a48be188ff8fadc811c10c61e88423d47b56d834
Facronactz <facronactz@gmail.com>
2022-06-13 23:08:54 +0700

    test slick

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -501,7 +501,8 @@
       $('.multiple-items').slick({
           infinite: true,
           slidesToShow: 3,
-          slidesToScroll: 3
+          slidesToScroll: 3,
+          nextArrow: <button type="button" class="carousel-icon-next">Next</button>
       });
     });
     </script>
