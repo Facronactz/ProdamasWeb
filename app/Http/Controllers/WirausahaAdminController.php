2e709ac9f77e7db545af26dc3a8f4302800b9687
agatabimaa <agatabima01@gmail.com>
2022-06-13 15:18:39 +0700

    wirausaha admin controller

---
+++ app/Http/Controllers/WirausahaAdminController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Eboost;
+use App\Models\DescriptionAdmin;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class WirausahaAdminController extends Controller
+{
+    public function create()
+    {
+        return view('admin.wirausaha.add');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'tentang' => 'required',
+            'foto_tentang' => 'required',
+            'info' => 'required',
+            'foto_info' => 'required',
+        ]);
+
+
+        $file=$request->file('foto_tentang');
+        $file2=$request->file('foto_info');
+        $foto = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $file->getClientOriginalName());
+        $foto2 = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $file2->getClientOriginalName());
+                $file->move(public_path('../fotoProd/'), $foto);
+                $file2->move(public_path('../fotoProd/'), $foto2);
+                Wirausaha::insert( [
+                    'tentang' => $request->tentang,
+                    'foto_tentang' => $request->foto_tentang,
+                    'foto_tentang' => $foto,
+                    'info' => $request->info,
+                    'foto_info' => $foto2,
+                ]);
+
+
+        return redirect('/admin/list-wirausaha')->with('success', 'Wirausaha Berhasil Ditambahkan!');
+    }
+
+    public function index()
+    {
+        $wirausaha = Wirausaha::groupBy('judul_tentang','judul_info')->get();
+        $descriptions = DescriptionAdmin::first()->get();
+        return view('admin.wirausaha.list', compact('wirausaha', 'descriptions'));
+    }
+
+    public function edit($id)
+    {
+        $wirausaha = Wirausaha::findOrFail($id);
+        return view('admin.wirausaha.edit', compact('wirausaha'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $this->validate($request, [
+            'tentang' => 'required',
+            'foto_tentang' => 'required',
+            'info' => 'required',
+            'foto_info' => 'required',
+        ]);
+
+        $wirausaha = Wirausaha::findorfail($id);
+        $file = $request->file('foto_tentang');
+        $file2 = $request->file('foto_info');
+        if ($file != NULL) {
+           $file= $request->file('foto_tentang');
+           $file2= $request->file('foto_info');
+            $foto = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $request->file('upload_foto_tentang')->getClientOriginalName(). '.'. $request->file('foto_tentang')->getClientOriginalExtension());
+            $foto2 = round(microtime(true) * 1000) . '-' . str_replace(' ', '-', $request->file('upload_foto_info')->getClientOriginalName(). '.' . $request->file('foto_info')->getClientOriginalExtension());
+                    $request->file('foto_tentang')->move(public_path('../fotoProd/'), $foto);
+                    $request->file('foto_info')->move(public_path('../fotoProd/'), $foto2);
+            
+
+            $wirausaha->update([
+                'tentang' => $request->tentang,
+                'foto_tentang' => $request->foto_tentang,
+                'foto_tentang' => $foto,
+                'info' => $request->info,
+                'foto_info' => $foto2,
+            ]);
+        } else {
+            $wirausaha->update([
+                'tentang' => $request->tentang,
+                'caption_info' => $request->caption_info,
+            ]);
+        }
+
+        return redirect('/admin/list-wirausaha')->with('success', 'Wirausaha Berhasil Diupdate!');
+    }
+
+    public function destroy($id)
+    {
+        $submission = DB::table('wirausaha')->where('id', $id)->delete();
+        return redirect('/admin/list-wirausaha')->with('success', 'Wirausaha Berhasil Dihapus!');
+    }
+
+}
