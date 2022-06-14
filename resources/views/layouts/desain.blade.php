8db066a7dd3ffbdb9bc48da6e8c330e6c7dd8b22
Facronactz <facronactz@gmail.com>
2022-06-14 11:07:29 +0700

    fix link data

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -455,7 +455,8 @@
                                     <!-- <a class="footer-link" href="{{ url('kritik') }}">Kritik & Saran</a> <br> -->
                                     {{-- <a class="footer-link" href="{{ url('grafik') }}">Prodamas Dalam Grafik</a> <br /> --}}
                                     <a class="footer-link" href="{{ url('peta') }}">Prodamas Dalam Peta</a> <br>
-                                    <a class="footer-link" href="{{ url('banksampah') }}">Bank Sampah</a>
+                                    <a class="footer-link" href="{{ url('banksampah') }}">Bank Sampah</a> <br>
+                                    <a class="footer-link" href="{{ url('msib') }}">Tim Pengembang</a>
                                 </div>
                                 <div class="col-md-6 col-lg-6">
                                     <p class="foot-title">
