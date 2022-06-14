531dc5734180fa4695d0cf74a82867bb0c14cc1a
Facronactz <facronactz@gmail.com>
2022-06-14 14:31:16 +0700

    test admin abc

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 @echo $user
-@break
+@stop
 <aside class="main-sidebar sidebar-light-primary elevation-4" style="background-color: #d3c4e2">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
