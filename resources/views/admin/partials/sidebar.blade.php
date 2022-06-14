9f8b4c5ce2d763a674d1a386305e28618defdce9
Facronactz <facronactz@gmail.com>
2022-06-14 23:20:17 +0700

    test admin menu

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -5,36 +5,55 @@
         <span class="brand-text font-weight-bold">PRODAMAS</span>
     </a>
     @php
-    $user = DB::table('users')->where('id','=', Auth::user()->id)->first();
-    $level = $user->level;
+        $user = DB::table('users')
+            ->where('id', '=', Auth::user()->id)
+            ->first();
+        $level = $user->level;
+        
+        $menus = DB::table('admin_menu')->get();
     @endphp
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="info">
-                <a href="#" class="d-block text-wrap">Selamat Datang {{$user -> name}}</a>
+                <a href="#" class="d-block text-wrap">Selamat Datang {{ $user->name }}</a>
             </div>
         </div>
-        
+
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                {{-- @if($user->level == 'super') --}}
-                @if($level == 'super')
-                <li class="nav-item">
-                    <a href="{{ url('/admin/list-menu') }}" class="nav-link @yield('menu')">
-                        <i class="nav-icon fas fa-bars"></i>
-                        <p>Menu</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ url('/admin/list-carousel') }}" class="nav-link @yield('carousel')">
-                        <i class="nav-icon fas fa-archway"></i>
-                        <p>Carousel Header</p>
-                    </a>
-                </li>
+                {{-- @if ($user->level == 'super') --}}
+
+                @foreach ($menus as $menu)
+                    @if ($level == 'super' || $menu->level == 'basic' || $menu->level == $level)
+                        <li class="nav-item">
+                            <a href="{{ $menu->link }}" class="nav-link @yield($menu->yield)">
+                                <i class="nav-icon {{ $menu->icon }}"></i>
+                                <p>{ $menu->name }}</p>
+                            </a>
+                        </li>
+                        {{-- <div class="card text-center col-sm-5 col-md-4 col-lg-3 col-xl-2 mx-2" style="background: {{ $menu->color }}; min-width: 200px;">
+                            <div class="card-body row">
+                                <div class="m-auto" style="color: #f4f6f9;">
+                                    <i class="{{ $menu->icon }} fa-7x"></i>
+                                    <a href="{{ $menu->link }}" class="stretched-link"></a>
+                                    <h2>{{ $menu->name }}</h2>
+                                </div>
+                            </div>
+                        </div> --}}
+                    @endif
+                @endforeach
+
+                {{-- @if ($level == 'super')
+                    <li class="nav-item">
+                        <a href="{{ url('/admin/list-carousel') }}" class="nav-link @yield('carousel')">
+                            <i class="nav-icon fas fa-archway"></i>
+                            <p>Carousel Header</p>
+                        </a>
+                    </li>
                 @endif
                 <li class="nav-item">
                     <a href="/admin/list-article" class="nav-link @yield('artikel')">
@@ -42,12 +61,12 @@
                         <p>Artikel</p>
                     </a>
                 </li>
-                {{-- <li class="nav-item">
+                <li class="nav-item">
                     <a href="/admin/submission" class="nav-link @yield('cerita')">
                         <i class="nav-icon fas fa-user-edit"></i>
                         <p>Tulis Cerita</p>
                     </a>
-                </li> --}}
+                </li>
                 <li class="nav-item">
                     <a href="/admin/list-foto" class="nav-link @yield('foto')">
                         <i class="nav-icon far fa-images"></i>
@@ -66,70 +85,70 @@
                         <p>Audio</p>
                     </a>
                 </li>
-                @if($level == 'super')
-                <li class="nav-item">
-                    <a href="/admin/list-prodamas" class="nav-link @yield('prodamas')">
-                        <i class="nav-icon fas fa-landmark"></i>
-                        <p>Prodamas</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-kampungkeren" class="nav-link @yield('kampungkeren')">
-                        <i class="nav-icon fas fa-city"></i>
-                        <p>Kampung Keren</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-bidang" class="nav-link @yield('bidang')">
-                        <i class="nav-icon fas fa-bezier-curve"></i></i>
-                        <p>Bidang</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-pokmas" class="nav-link @yield('pokmas')">
-                        <i class="nav-icon fas fa-users"></i>
-                        <p>Pokmas</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-tuliscerita" class="nav-link @yield('tuliscerita')">
-                        <i class="nav-icon fas fa-edit"></i>
-                        <p>Tulis Cerita</p>
-                    </a>
-                </li>
+                @if ($level == 'super')
+                    <li class="nav-item">
+                        <a href="/admin/list-prodamas" class="nav-link @yield('prodamas')">
+                            <i class="nav-icon fas fa-landmark"></i>
+                            <p>Prodamas</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-kampungkeren" class="nav-link @yield('kampungkeren')">
+                            <i class="nav-icon fas fa-city"></i>
+                            <p>Kampung Keren</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-bidang" class="nav-link @yield('bidang')">
+                            <i class="nav-icon fas fa-bezier-curve"></i></i>
+                            <p>Bidang</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-pokmas" class="nav-link @yield('pokmas')">
+                            <i class="nav-icon fas fa-users"></i>
+                            <p>Pokmas</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-tuliscerita" class="nav-link @yield('tuliscerita')">
+                            <i class="nav-icon fas fa-edit"></i>
+                            <p>Tulis Cerita</p>
+                        </a>
+                    </li>
                 @endif
-                @if($level == 'super'||$level == 'sospem')
-                <li class="nav-item">
-                    <a href="/admin/list-eboost" class="nav-link @yield('eboost')">
-                        <i class="nav-icon fas fa-chart-line"></i>
-                        <p>E-Boost</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-umkm" class="nav-link @yield('umkm')">
-                        <i class="nav-icon fas fa-house-user"></i>
-                        <p>UMKM</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-koperasirw" class="nav-link @yield('koperasirw')">
-                        <i class="nav-icon fas fa-store-alt"></i>
-                        <p>Koperasi RW</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-wirausaha" class="nav-link @yield('wirausaha')">
-                        <i class="nav-icon fas fa-address-card"></i>
-                        <p>Wirausaha</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/admin/list-kube" class="nav-link @yield('kube')">
-                        <i class="nav-icon fas fa-edit"></i>
-                        <p>Kube</p>
-                    </a>
-                </li>
-                @endif
+                @if ($level == 'super' || $level == 'sospem')
+                    <li class="nav-item">
+                        <a href="/admin/list-eboost" class="nav-link @yield('eboost')">
+                            <i class="nav-icon fas fa-chart-line"></i>
+                            <p>E-Boost</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-umkm" class="nav-link @yield('umkm')">
+                            <i class="nav-icon fas fa-house-user"></i>
+                            <p>UMKM</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-koperasirw" class="nav-link @yield('koperasirw')">
+                            <i class="nav-icon fas fa-store-alt"></i>
+                            <p>Koperasi RW</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-wirausaha" class="nav-link @yield('wirausaha')">
+                            <i class="nav-icon fas fa-address-card"></i>
+                            <p>Wirausaha</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/admin/list-kube" class="nav-link @yield('kube')">
+                            <i class="nav-icon fas fa-edit"></i>
+                            <p>Kube</p>
+                        </a>
+                    </li>
+                @endif --}}
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
