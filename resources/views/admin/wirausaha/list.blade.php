e30404f00360202063a51a36c704652e6c02fac1
achieto <anggie.t17210@gmail.com>
2022-06-16 14:40:18 +0700

    Wira Wira

--- resources/views/admin/wirausaha/list.blade.php
+++ resources/views/admin/wirausaha/list.blade.php
@@ -41,9 +41,9 @@ Wirausaha Submission
         @forelse ($wirausaha as $key=>$wirausahas)
         <tr>
             <td>{{$key + 1}}</td>
-            <td><?=substr($wirausahas->tentang, 0, 100)?></td>
-            <td>{{$wirausahas->info}}</td>
-            <td>{{$wirausahas->deskripsi}}</td>
+            <td><?=$wirausahas->tentang?></td>
+            <td><?=$wirausahas->info?></td>
+            <td><?=$wirausahas->deskripsi?></td>
             <td>
                 <form action="/admin/edit-wirausaha/{{$wirausahas->id}}" method="POST">
                     <a href="/admin/edit-wirausaha/{{$wirausahas->id}}" class="btn btn-info">Edit</a>
