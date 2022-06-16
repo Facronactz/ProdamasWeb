125c9c7f7b929430461dc9897a53da8240e8c08a
qurrotulayunina <qanina00@gmail.com>
2022-06-16 15:33:10 +0700

    fix wirausaha index 3

--- resources/views/wirausaha/index.blade.php
+++ resources/views/wirausaha/index.blade.php
@@ -29,6 +29,23 @@
                 @endforeach
             </div>
         </div>
+
+        <hr>
+        <div class="card border-light my-4">
+            <div class="row g-0">
+                <div class="col-lg-4 d-flex justify-content-center">
+                    <img src="{{ asset('wirausahaProd/'.$e->foto_info) }}" class="img-fluid" style="max-height: 400px;">
+                </div>
+                <div class="col-lg-8">
+                    <div class="card-body">
+                        <h2 class="card-title">Informasi Wirausaha</h2>
+                        <div style="height: 2px; background-color:#000000"></div>
+                        <p class="card-text align-middle" style="font-size: 20px"><?= $e->info ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </div>
 
