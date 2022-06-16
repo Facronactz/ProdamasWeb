decb8fcb164494e8c22bf95feb70f3975c57ebb0
qurrotulayunina <qanina00@gmail.com>
2022-06-16 15:58:34 +0700

    fix wirausaha index 14

--- resources/views/wirausaha/index.blade.php
+++ resources/views/wirausaha/index.blade.php
@@ -40,7 +40,7 @@
                     <div class="card-body">
                         <h2 class="card-title">Informasi Wirausaha</h2>
                         <div style="height: 2px; background-color:#000000"></div>
-                        <span class="card-text align-middle" style="font-size: 20px"><?= $e->info ?></span>
+                        <?= $e->info ?>
                     </div>
                 </div>
             </div>
