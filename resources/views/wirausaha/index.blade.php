b10f089a0a37a5f9e30aeb4db7b491f009f9d336
qurrotulayunina <qanina00@gmail.com>
2022-06-16 15:27:43 +0700

    fix wirausaha index

--- resources/views/wirausaha/index.blade.php
+++ resources/views/wirausaha/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.desain')
 
 @section('content')
-{{-- <div>
+<div>
     <img class="img-fluid" src="{{asset('kubeProd/KUBE.png')}}">
 </div>
 <div class="container">
@@ -12,44 +12,7 @@
                 di Kota Kediri.
             </h4>
         </div>
-        <div class="card border-light my-4">
-            <div class="row g-0">
-                @foreach ($wirausaha as $e)
-                <div class="col-lg-4 d-flex justify-content-center">
-                    <img src="{{ asset('wirausahaProd/'.$e->foto_tentang) }}" class="img-fluid" style="max-height: 400px;">
-                </div>
-                <div class="col-lg-8">
-                    <div class="card-body">
-                        <h2 class="card-title">Tentang Wirausaha</h2>
-                        <div style="height: 2px; background-color:#000000"></div>
-                        <p class="card-text align-middle" style="font-size: 20px"><?= $e->tentang ?></p>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-        </div>
-        <hr>
-        <div class="card border-light my-4">
-            <div class="row g-0">
-                <div class="col-lg-4 d-flex justify-content-center">
-                    <img src="{{ asset('wirausahaProd/'.$e->foto_info) }}" class="img-fluid" style="max-height: 400px;">
-                </div>
-                <div class="col-lg-8">
-                    <div class="card-body">
-                        <h2 class="card-title">Informasi Wirausaha</h2>
-                        <div style="height: 2px; background-color:#000000"></div>
-                        <p class="card-text align-middle" style="font-size: 20px"><?= $e->info ?></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="pt2" style="font-size: 20px; text-align:justify; text-indent: 25px">
-            @foreach ($wirausaha as $e)
-            <?= $e->deskripsi ?>
-            @endforeach
-        </div>
     </div>
-</div> --}}
+</div>
 
 @endsection
