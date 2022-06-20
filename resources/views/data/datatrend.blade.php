57e699ffc61dd542f3318770eb1a6b5091809594
Facronactz <facronactz@gmail.com>
2022-06-20 14:25:10 +0700

    fix trend

--- resources/views/data/datatrend.blade.php
+++ resources/views/data/datatrend.blade.php
@@ -58,7 +58,7 @@
                 var divElement = document.getElementById('viz1655393574272');
                 var vizElement = divElement.getElementsByTagName('object')[0];
                 vizElement.style.width = '100%';
-                vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    
+                vizElement.style.height=(divElement.offsetWidth*0.5)+'px';                    
                 // vizElement.style.height = '800px';
                 var scriptElement = document.createElement('script');
                 scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
