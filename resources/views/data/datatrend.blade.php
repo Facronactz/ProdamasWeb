a0a8b625a3822e502fb3fd5d5ab7b0956016481d
Facronactz <facronactz@gmail.com>
2022-06-20 13:51:58 +0700

    fix trend

--- resources/views/data/datatrend.blade.php
+++ resources/views/data/datatrend.blade.php
@@ -70,7 +70,7 @@
         var divElement = document.getElementById('viz1655707617330');
         var vizElement = divElement.getElementsByTagName('object')[0];
         vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+        vizElement.style.height = '800px';
         var scriptElement = document.createElement('script');
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
         vizElement.parentNode.insertBefore(scriptElement, vizElement);
