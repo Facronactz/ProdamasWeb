798c62f1b7e17d411e30b4402ee8ae94594dcc61
achieto <anggie.t17210@gmail.com>
2022-06-20 15:02:24 +0700

    Anggaran CRUD

--- routes/web.php
+++ routes/web.php
@@ -319,10 +319,10 @@ Route::group(['middleware' => 'auth'], function () { //agar tidak dapat tampil m
     Route::delete('/admin/edit-kurnia/{kurnias_id}', [KurniaAdminController::class, 'destroy']);
 
     // Anggaran
-    Route::get('/admin/edit-anggaran/{anggaran_id}', [AnggaranAdminController::class, 'edit']);
-    Route::put('/admin/edit-anggaran/{anggaran_id}', [AnggaranAdminController::class, 'update']);
+    Route::get('/admin/anggaran/{anggaran_id}', [AnggaranAdminController::class, 'edit']);
+    Route::put('/admin/anggaran/{anggaran_id}', [AnggaranAdminController::class, 'update']);
     Route::get('/admin/list-anggaran', [AnggaranAdminController::class, 'index']);
-    Route::delete('/admin/edit-anggaran/{anggaran_id}', [AnggaranAdminController::class, 'destroy']);
+    Route::delete('/admin/anggaran/{anggaran_id}', [AnggaranAdminController::class, 'destroy']);
 });
 
 //berlangganan
