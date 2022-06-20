ae23521a5107af6dc81d02e503aa17b469f30a7f
agatabimaa <agatabima01@gmail.com>
2022-06-20 16:05:58 +0700

    model english

--- app/Models/EnglishMasive.php
+++ app/Models/EnglishMasive.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
-class Anggaran extends Model
+class EnglishMasive extends Model
 {
     use HasFactory;
     protected $table = "english_masive";
