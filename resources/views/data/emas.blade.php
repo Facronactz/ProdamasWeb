651770c9c60dc69f19f2fd80b8465fbb1da36b8a
agatabimaa <agatabima01@gmail.com>
2022-06-20 18:11:00 +0700

    emas

--- resources/views/data/emas.blade.php
+++ resources/views/data/emas.blade.php
@@ -2,102 +2,25 @@
 @section('data', 'active')
 @section('emas', 'active')
 
-@section('title', 'Prodamas Dalam Peta')
+@section('title', 'Prodamas Dalam Angka')
 
 @section('head')
-<link rel="stylesheet" href="css/peta.css">
+{{-- <link rel="stylesheet" href="css/peta.css"> --}}
 @endsection
 
 @section('content')
-{{-- <div  class='row' > --}}
-<div class='tableauPlaceholder' id='viz1655361597858' style='position: relative; padding:0 50px; margin:50px 0'>
-    <div class='tableauPlaceholder' id='viz1655363034029' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tu&#47;Tutor_EMAS&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'>
-            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-            <param name='embed_code_version' value='3' />
-            <param name='site_root' value='' />
-            <param name='name' value='Tutor_EMAS&#47;Dashboard1' />
-            <param name='tabs' value='no' />
-            <param name='toolbar' value='yes' />
-            <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tu&#47;Tutor_EMAS&#47;Dashboard1&#47;1.png' />
-            <param name='animate_transition' value='yes' />
-            <param name='display_static_image' value='yes' />
-            <param name='display_spinner' value='yes' />
-            <param name='display_overlay' value='yes' />
-            <param name='display_count' value='yes' />
-            <param name='language' value='en-US' />
-        </object>
+<div style="padding:0 50px; margin:50px 0;">
+    @foreach($dataemas as $emas)
+    <div style="text-align:center">
+        <h2 style="text-decoration:underline">{{$emas->judul}}</h2>
+        <p style="font-weight:700">{{$emas->deskripsi}}</p>
     </div>
-</div>
-<script type='text/javascript'>
-    var divElement = document.getElementById('viz1655363034029');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) {
-        vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    } else {
-        vizElement.style.width = '100%';
-        vizElement.style.height = '727px';
-    }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-</script>
 
-<div class='tableauPlaceholder' id='viz1655361597858' style='position: relative; padding:0 50px; margin:50px 0'>
-    <div class='tableauPlaceholder' id='viz1655363206794' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ju&#47;JumlahPartisipanEMAS&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'>
-            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
-            <param name='embed_code_version' value='3' />
-            <param name='site_root' value='' />
-            <param name='name' value='JumlahPartisipanEMAS&#47;Dashboard1' />
-            <param name='tabs' value='no' />
-            <param name='toolbar' value='yes' />
-            <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Ju&#47;JumlahPartisipanEMAS&#47;Dashboard1&#47;1.png' />
-            <param name='animate_transition' value='yes' />
-            <param name='display_static_image' value='yes' />
-            <param name='display_spinner' value='yes' />
-            <param name='display_overlay' value='yes' />
-            <param name='display_count' value='yes' />
-            <param name='language' value='en-US' />
-        </object>
+    <div>
+        <?= $emas->tableau ?>
     </div>
+    <div class="mt-3"><?=$emas->konten?></div>
+    <div style="font-size:small">(<em>Sumber Data</em>: {{$emas->sumber}})</div>
+    @endforeach
 </div>
-<script type='text/javascript'>
-    var divElement = document.getElementById('viz1655363206794');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) {
-        vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    } else {
-        vizElement.style.width = '100%';
-        vizElement.style.height = '727px';
-    }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-</script>
-{{-- </div> --}}
-
-<script type='text/javascript'>
-    var divElement = document.getElementById('viz1642533848968');
-    var vizElement = divElement.getElementsByTagName('object')[0];
-    if (divElement.offsetWidth > 800) {
-        vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width = '100%';
-        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-    } else {
-        vizElement.style.width = '100%';
-        vizElement.style.height = '727px';
-    }
-    var scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    vizElement.parentNode.insertBefore(scriptElement, vizElement);
-</script>
 @endsection
