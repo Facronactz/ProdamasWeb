d224d1f0a70cc7705334f8aaac483f74c2cae2d7
Facronactz <facronactz@gmail.com>
2022-06-20 15:15:28 +0700

    fix trend

--- resources/views/data/datatrend.blade.php
+++ resources/views/data/datatrend.blade.php
@@ -99,10 +99,10 @@
         var vizElement = divElement.getElementsByTagName('object')[0];
         if (divElement.offsetWidth > 800) {
             vizElement.style.width = '100%';
-            vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+            vizElement.style.height = (divElement.offsetWidth * 0.4) + 'px';
         } else if (divElement.offsetWidth > 500) {
             vizElement.style.width = '100%';
-            vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+            vizElement.style.height = (divElement.offsetWidth * 0.4) + 'px';
         } else {
             vizElement.style.width = '100%';
             vizElement.style.height = '727px';
