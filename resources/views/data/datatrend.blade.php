21ea03d94424e888112ac0ea71753b533a40d8cb
Facronactz <facronactz@gmail.com>
2022-06-20 13:48:04 +0700

    fix trend

--- resources/views/data/datatrend.blade.php
+++ resources/views/data/datatrend.blade.php
@@ -13,7 +13,7 @@
     <div class="container-md my-3">
         <select id="selector" class="form-select" aria-label="datatrend selector">
             <option value="viz1655393574272" selected>Data Trend Anggaran, Serapan, dan Sisa Kota Kediri</option>
-            <option value="viz1655395412741">Data Trend Anggaran, Serapan, dan Sisa Kota Kediri Per Kecamatan</option>
+            <option value="viz1655707617330">Data Trend Anggaran, Serapan, dan Sisa Kota Kediri Per Kecamatan</option>
             <option value="viz1655395721778">Data Trend Anggaran, Serapan, dan Sisa Kota Kediri Per Kelurahan</option>
         </select>
     </div>
@@ -50,8 +50,34 @@
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
         vizElement.parentNode.insertBefore(scriptElement, vizElement);
     </script>
+    {{--  --}}
+    <div class='tableauPlaceholder' id='viz1655707617330' style='position: relative'><noscript><a href='#'><img alt='Data Trend Anggaran, Serapan, dan Sisa Per Kecamatan di Kota Kediri ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Trend_Per_Kecamatan&#47;Sheet1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz' style='display:none;'>
+            <param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+            <param name='embed_code_version' value='3' />
+            <param name='site_root' value='' />
+            <param name='name' value='Trend_Per_Kecamatan&#47;Sheet1' />
+            <param name='tabs' value='no' />
+            <param name='toolbar' value='yes' />
+            <param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Trend_Per_Kecamatan&#47;Sheet1&#47;1.png' />
+            <param name='animate_transition' value='yes' />
+            <param name='display_static_image' value='yes' />
+            <param name='display_spinner' value='yes' />
+            <param name='display_overlay' value='yes' />
+            <param name='display_count' value='yes' />
+            <param name='language' value='en-US' />
+        </object></div>
+    <script type='text/javascript'>
+        var divElement = document.getElementById('viz1655707617330');
+        var vizElement = divElement.getElementsByTagName('object')[0];
+        vizElement.style.width = '100%';
+        vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
+        var scriptElement = document.createElement('script');
+        scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+        vizElement.parentNode.insertBefore(scriptElement, vizElement);
+    </script>
+    {{--  --}}
 
-    <div class='tableauPlaceholder' id='viz1655395412741' style='position: relative; padding:0 50px; margin:50px 0'>
+    {{-- <div class='tableauPlaceholder' id='viz1655395412741' style='position: relative; padding:0 50px; margin:50px 0'>
         <noscript>
             <a href='#'>
                 <img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Tr&#47;Trend_Per_Kecamatan&#47;Dashboard1&#47;1_rss.png' style='border: none' />
@@ -87,11 +113,11 @@
         //     vizElement.style.height = '727px';
         // }
         vizElement.style.width = '100%';
-        vizElement.style.height='800px';
+        vizElement.style.height = '800px';
         var scriptElement = document.createElement('script');
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
         vizElement.parentNode.insertBefore(scriptElement, vizElement);
-    </script>
+    </script> --}}
 
     <div class='tableauPlaceholder' id='viz1655395721778' style='position: relative; padding:0 50px; margin:50px 0'> <noscript>
             <a href='#'>
@@ -119,7 +145,7 @@
         var vizElement = divElement.getElementsByTagName('object')[0];
         vizElement.style.width = '100%';
         // vizElement.style.height = (divElement.offsetWidth * 0.75) + 'px';
-        vizElement.style.height='800px';
+        vizElement.style.height = '800px';
         var scriptElement = document.createElement('script');
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
         vizElement.parentNode.insertBefore(scriptElement, vizElement);
@@ -135,7 +161,7 @@
             $('#selector').val('viz1655393574272')
 
             sessionStorage.setItem("datatrend", 'viz1655393574272');
-            $('#viz1655395412741').addClass('visually-hidden');
+            $('#viz1655707617330').addClass('visually-hidden');
             $('#viz1655395721778').addClass('visually-hidden');
         });
 
