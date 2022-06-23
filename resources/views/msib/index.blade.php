26cc0a881b42a02842f06c1d8713a4fb1fd72d51
Facronactz <facronactz@gmail.com>
2022-06-23 16:37:22 +0700

    add msib page

---
+++ resources/views/msib/index.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <!-- Font Awesome -->
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+    <style></style>
+</head>
+
+<body>
+    <div class="container-xl">
+        <div class="row text-center">
+            <h1 class="display-3">Tim Magang Merdeka Batch 2<br> Pemerintahan Kota Kediri</h1>
+            <h1 class="mt-3">Website dan Aplikasi</h1>
+            <small class="text-muted">In Website We Trust</small>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-2">
+                <div class="col col-lg col-md-12">
+                    <img style="border-radius: 50%;" src="https://via.placeholder.com/400x400?text=Poto" alt="" srcset="">
+                    <h3>Farro</h3>
+                    <span>Jawa Timur</span><br>
+                    <span>Teknik Informatika</span><br>
+                    <span>Universitas Negeri Surabaya </span><br>
+                    <a href="" class="text-black"><i class="fab fa-instagram"></i></a>
+                    <a href="" class="text-black"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col mb-2">
+                    <img style="border-radius: 50%;" src="https://via.placeholder.com/400x400?text=Poto" alt="" srcset="">
+                    <h3>Burhan</h3>
+                    <span>Jawa Timur</span><br>
+                    <span>Teknik Informatika</span><br>
+                    <span>Universitas Negeri Surabaya </span><br>
+                    <a href="" class="text-black"><i class="fab fa-instagram"></i></a>
+                    <a href="" class="text-black"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col mb-2">
+                    <img style="border-radius: 50%;" src="https://via.placeholder.com/400x400?text=Poto" alt="" srcset="">
+                    <h3>Nina</h3>
+                    <span>Jawa Timur</span><br>
+                    <span>Teknik Informatika</span><br>
+                    <span>Universitas Negeri Surabaya </span><br>
+                    <a href="" class="text-black"><i class="fab fa-instagram"></i></a>
+                    <a href="" class="text-black"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col mb-2">
+                    <img style="border-radius: 50%;" src="https://via.placeholder.com/400x400?text=Poto" alt="" srcset="">
+                    <h3>Anggie</h3>
+                    <span>Jawa Timur</span><br>
+                    <span>Teknik Informatika</span><br>
+                    <span>Universitas Negeri Surabaya </span><br>
+                    <a href="" class="text-black"><i class="fab fa-instagram"></i></a>
+                    <a href="" class="text-black"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col mb-2">
+                    <img style="border-radius: 50%;" src="https://via.placeholder.com/400x400?text=Poto" alt="" srcset="">
+                    <h3>Bima</h3>
+                    <span>Jawa Timur</span><br>
+                    <span>Teknik Informatika</span><br>
+                    <span>Universitas Negeri Surabaya </span><br>
+                    <a href="" class="text-black"><i class="fab fa-instagram"></i></a>
+                    <a href="" class="text-black"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Script-->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
