fea0a23637e9e2fc1c1b2d0af683b3bfd8006967
achieto <anggie.t17210@gmail.com>
2022-07-03 19:04:28 +0700

    coba ya nanti apus lagi

--- resources/views/layouts/desain.blade.php
+++ resources/views/layouts/desain.blade.php
@@ -351,7 +351,7 @@
                             <a href="{{ url($menus->link) }}" target="_blank" style="color:white" class="nav-link nav-list @yield('{{ $menus->judul }}')">{{ $menus->judul }}</a>
                             @empty
                             <?= ""?>
-                            @endif
+                            @endempty
                             @endforeach
                             <div class="nav-list" id="scrollspy">
                                 <a class="nav-link" href="#footer">Kontak</a>
