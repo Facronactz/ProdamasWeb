027cd30d8c87d39cfa6e52bf7c0b1bb2e6726edc
achieto <anggie.t17210@gmail.com>
2022-07-03 18:55:55 +0700

    coba ya nanti apus lagi

--- app/Http/Controllers/KubeController.php
+++ app/Http/Controllers/KubeController.php
@@ -28,6 +28,7 @@ class KubeController extends Controller
         ->sum('views');
 
         $menus = Menu::where('status', 'Show')->get();
+        $menu = Menu::where('status', 'Hidden')->get();
         $picts = Pict::first()->get();
         $kube = Kube::first()->get();
 
